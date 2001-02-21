e54cd522bd542e4477d4d505c649e9ac30c2c566
Bear <Bear@8cecbe3e-ef64-0410-a610-ece5afc49ac1>
2001-02-21 00:26:15 +0000

    added test codes
    git-svn-id: https://svn.dev.java.net/svn/msv/trunk@60 8cecbe3e-ef64-0410-a610-ece5afc49ac1

--- xsdlib/test/com/sun/msv/datatype/datetime/BigDateTimeValueTypeTest.java
+++ xsdlib/test/com/sun/msv/datatype/datetime/BigDateTimeValueTypeTest.java
@@ -1,19 +1,18 @@
 package com.sun.tranquilo.datatype.datetime;
+
 import java.math.BigInteger;
-
 import java.math.BigDecimal;
-
 import com.sun.tranquilo.datatype.Comparator;
-
 import junit.framework.*;
+import java.io.ByteArrayInputStream;
 
 public class BigDateTimeValueTypeTest extends TestCase {    
 	
-	public BigDateTimeValueTypeTest(java.lang.String testName) {
+	public BigDateTimeValueTypeTest(String testName) {
 		super(testName);
 	}
 	
-	public static void main(java.lang.String[] args) {
+	public static void main(String[] args) {
 		junit.textui.TestRunner.run(suite());
 	}
 	
@@ -21,40 +20,53 @@ public class BigDateTimeValueTypeTest extends TestCase {
 		return new TestSuite(BigDateTimeValueTypeTest.class);
 	}
 	
+	private ISO8601Parser getParser( String s ) throws Exception
+	{
+		return new ISO8601Parser(new ByteArrayInputStream(s.getBytes("UTF-8")));
+	}
+	
 	/** Test of getBigValue method, of class com.sun.tranquilo.datatype.datetime.BigDateTimeValueType. */
-	public void testGetBigValue() {
-		System.out.println("testGetBigValue");
-		// Add your test code here.
-	}
-	
-	/** Test of equals method, of class com.sun.tranquilo.datatype.datetime.BigDateTimeValueType. */
-	public void testEquals() {
-		System.out.println("testEquals");
-		// Add your test code here.
-	}
-	
-	/** Test of hashCode method, of class com.sun.tranquilo.datatype.datetime.BigDateTimeValueType. */
-	public void testHashCode() {
-		System.out.println("testHashCode");
-		// Add your test code here.
+	public void testGetBigValue()  throws Exception
+	{
+		BigDateTimeValueType t = (BigDateTimeValueType)getParser("2000-01").yearMonthTypeV();
+		assertEquals( t, t.getBigValue() );
 	}
 	
 	/** Test of compare method, of class com.sun.tranquilo.datatype.datetime.BigDateTimeValueType. */
 	public void testCompare() {
-		System.out.println("testCompare");
-		// Add your test code here.
 	}
 	
 	/** Test of normalize method, of class com.sun.tranquilo.datatype.datetime.BigDateTimeValueType. */
-	public void testNormalize() {
-		System.out.println("testNormalize");
-		// Add your test code here.
+	public void testNormalize() throws Exception
+	{
+		BigDateTimeValueType v;
+		
+		v = (BigDateTimeValueType)getParser("2000-03-04T23:00:00-03").dateTimeTypeV().normalize();
+		
+		// equals method compares two by calling normalize,
+		// so actually this cannot be said as a testing.
+		assertEquals( v,
+			getParser("2000-03-05T02:00:00Z").dateTimeTypeV() );
 	}
 	
 	/** Test of add method, of class com.sun.tranquilo.datatype.datetime.BigDateTimeValueType. */
-	public void testAdd() {
-		System.out.println("testAdd");
-		// Add your test code here.
+	public void testAdd() throws Exception
+	{
+		BigDateTimeValueType v;
+		
+		// from examples of Appendix.E of the spec.
+		
+		v = getParser("2000-01-12T12:13:14Z").dateTimeTypeV().add(
+				getParser("P1Y3M5DT7H10M3.3S").durationTypeV() ).getBigValue();
+		assertEquals( v, getParser("2001-04-17T19:23:17.3Z").dateTimeTypeV() );
+		
+		v = getParser("2000-01").yearMonthTypeV().add(
+				getParser("-P3M").durationTypeV() ).getBigValue();
+		assertEquals( v, getParser("1999-10").yearMonthTypeV() );
+		
+		v = getParser("2000-01-12-05").dateTypeV().add(
+				getParser("PT33H").durationTypeV() ).getBigValue();
+		assertEquals( v, getParser("2000-01-13-05").dateTypeV() );
 	}
 	
 }

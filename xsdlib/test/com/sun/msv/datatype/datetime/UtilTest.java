483aa3d541815eb8e5ad44b95719b49419358c71
Bear <Bear@8cecbe3e-ef64-0410-a610-ece5afc49ac1>
2001-02-21 19:19:16 +0000

    test cases added
    git-svn-id: https://svn.dev.java.net/svn/msv/trunk@68 8cecbe3e-ef64-0410-a610-ece5afc49ac1

--- xsdlib/test/com/sun/msv/datatype/datetime/UtilTest.java
+++ xsdlib/test/com/sun/msv/datatype/datetime/UtilTest.java
@@ -1,52 +1,75 @@
 package com.sun.tranquilo.datatype.datetime;
+
 import java.math.BigInteger;
-
 import java.math.BigDecimal;
-
+import com.sun.tranquilo.datatype.Comparator;
 import junit.framework.*;
 
-public class UtilTest extends TestCase {    
+public class UtilTest extends TestCase
+{    
+	public UtilTest(String testName) { super(testName); }
 	
-	public UtilTest(java.lang.String testName) {
-		super(testName);
-	}
-	
-	public static void main(java.lang.String[] args) {
+	public static void main(String[] args)
+	{
 		junit.textui.TestRunner.run(suite());
 	}
 	
-	public static Test suite() {
+	public static Test suite()
+	{
 		return new TestSuite(UtilTest.class);
 	}
 	
 	/** Test of objEqual method, of class com.sun.tranquilo.datatype.datetime.Util. */
-	public void testObjEqual() {
-		System.out.println("testObjEqual");
-		// Add your test code here.
+	public void testObjEqual()
+	{
+		assertEquals( true,	Util.objEqual(null,null) );
+		assertEquals( false,Util.objEqual(this,null) );
+		assertEquals( false,Util.objEqual(null,this) );
+		assertEquals( true,	Util.objEqual(this,this) );
+		assertEquals( true,	Util.objEqual("12","12") );
 	}
 	
 	/** Test of objHashCode method, of class com.sun.tranquilo.datatype.datetime.Util. */
-	public void testObjHashCode() {
-		System.out.println("testObjHashCode");
-		// Add your test code here.
+	public void testObjHashCode()
+	{
+		assertEquals( this.hashCode(),	Util.objHashCode(this) );
+		assertEquals( Util.objHashCode(null), Util.objHashCode(null) );
 	}
 	
 	/** Test of objCompare method, of class com.sun.tranquilo.datatype.datetime.Util. */
-	public void testObjCompare() {
-		System.out.println("testObjCompare");
-		// Add your test code here.
+	public void testObjCompare()
+	{
+		assertEquals( Comparator.EQUAL,		Util.objCompare(null,null) );
+		assertEquals( Comparator.LESS,		Util.objCompare(new Integer(10), new Integer(20)) );
+		assertEquals( Comparator.GREATER,	Util.objCompare(new Integer(25), new Integer(20)) );
+		assertEquals( Comparator.UNDECIDABLE,Util.objCompare(null,new Integer(1)) );
+		assertEquals( Comparator.UNDECIDABLE,Util.objCompare(new Integer(1),null) );
 	}
 	
 	/** Test of int2bi method, of class com.sun.tranquilo.datatype.datetime.Util. */
-	public void testInt2bi() {
-		System.out.println("testInt2bi");
-		// Add your test code here.
+	public void testInt2bi()
+	{
+		assertEquals( Util.int2bi(15), new BigInteger("15") );
+		assertEquals( Util.int2bi(new Integer(15)), new BigInteger("15") );
 	}
 	
 	/** Test of maximumDayInMonthFor method, of class com.sun.tranquilo.datatype.datetime.Util. */
-	public void testMaximumDayInMonthFor() {
-		System.out.println("testMaximumDayInMonthFor");
-		// Add your test code here.
+	public void testMaximumDayInMonthFor()
+	{
+		int[] test = new int[]{
+			2000, 0/*Jan*/,		31,
+			2000, 1/*Feb*/,		29,
+			1996, 1/*Feb*/,		29,
+			1900, 1/*Feb*/,		28,
+			   0, 1/*Feb*/,		29,
+			-400, 1/*Feb*/,		29,
+			-397, 1/*Feb*/,		28 };
+			
+		for( int i=0; i<test.length; i+=3 )
+		{
+			assertEquals( test[i+2], Util.maximumDayInMonthFor(test[i],test[i+1]) );
+			assertEquals( test[i+2], Util.maximumDayInMonthFor(Util.int2bi(test[i]),test[i+1]) );
+		}
 	}
 	
 }

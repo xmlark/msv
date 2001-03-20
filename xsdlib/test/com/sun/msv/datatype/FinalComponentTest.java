3ba9f2c4356dcd2b56e695fcc6733ab29ebf0d59
Bear <Bear@8cecbe3e-ef64-0410-a610-ece5afc49ac1>
2001-03-20 20:38:24 +0000

    implemented "fixed" attribute semantics
    git-svn-id: https://svn.dev.java.net/svn/msv/trunk@138 8cecbe3e-ef64-0410-a610-ece5afc49ac1

---
+++ xsdlib/test/com/sun/msv/datatype/FinalComponentTest.java
@@ -0,0 +1,62 @@
+package com.sun.tranquilo.datatype;
+
+import junit.framework.*;
+
+public class FinalComponentTest extends TestCase
+{
+	public FinalComponentTest( String name ) { super(name); }
+	
+	public static void main(java.lang.String[] args) {
+		junit.textui.TestRunner.run(suite());
+	}
+	
+	public static Test suite() {
+		return new TestSuite(FinalComponentTest.class);
+	}
+	
+	public void testDerivationByRestriction() throws BadTypeException
+	{
+		TypeIncubator inc = new TypeIncubator(
+			new FinalComponent(
+				StringType.theInstance,
+				DataType.DERIVATION_BY_RESTRICTION ) );
+		
+		try
+		{
+			inc.derive("test");
+			fail("should throw Exception here");
+		}
+		catch( BadTypeException bte ) {;}
+	}
+	
+	public void testDerivationByList() throws BadTypeException
+	{
+		try
+		{
+			DataTypeFactory.deriveByList( "test",
+				DataTypeFactory.deriveByUnion("intermediate",
+				  new DataType[]{
+						new FinalComponent(
+							StringType.theInstance,
+							DataType.DERIVATION_BY_LIST ),
+						NumberType.theInstance } ) );
+			fail("should throw Exception here");
+		}
+		catch( BadTypeException bte ) {;}
+	}
+
+	public void testDerivationByUnion() throws BadTypeException
+	{
+		try
+		{
+			DataTypeFactory.deriveByUnion("intermediate",
+			  new DataType[]{
+					new FinalComponent(
+						StringType.theInstance,
+						DataType.DERIVATION_BY_UNION ),
+					NumberType.theInstance } );
+			fail("should throw Exception here");
+		}
+		catch( BadTypeException bte ) {;}
+	}
+}

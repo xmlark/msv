47f3ad6f3966ad6e11d26490727f838ea871253e
Bear <Bear@8cecbe3e-ef64-0410-a610-ece5afc49ac1>
2001-03-20 23:54:47 +0000

    added
    git-svn-id: https://svn.dev.java.net/svn/msv/trunk@150 8cecbe3e-ef64-0410-a610-ece5afc49ac1

---
+++ msv/src/com/sun/msv/util/DatatypeRef.java
@@ -0,0 +1,9 @@
+package com.sun.tranquilo.util;
+
+/**
+ * reference to {@link com.sun.tranquilo.datatype.DataType}
+ */
+public class DataTypeRef
+{
+	public com.sun.tranquilo.datatype.DataType type = null;
+}

c97a97ce1dc6006f4d4ecd12f341a5a15c564969
Bear <Bear@8cecbe3e-ef64-0410-a610-ece5afc49ac1>
2001-03-20 20:37:57 +0000

    implemented "fixed" attribute
    git-svn-id: https://svn.dev.java.net/svn/msv/trunk@137 8cecbe3e-ef64-0410-a610-ece5afc49ac1

---
+++ xsdlib/src/com/sun/msv/datatype/xsd/FinalComponent.java
@@ -0,0 +1,60 @@
+/*
+ * @(#)$Id$
+ *
+ * Copyright 2001 Sun Microsystems, Inc. All Rights Reserved.
+ * 
+ * This software is the proprietary information of Sun Microsystems, Inc.  
+ * Use is subject to license terms.
+ * 
+ */
+package com.sun.tranquilo.datatype;
+
+/**
+ * "final" component.
+ * 
+ * @author	Kohsuke Kawaguchi
+ */
+public final class FinalComponent extends DataTypeImpl
+{
+	/** immediate base type, which may be a concrete type or DataTypeWithFacet */
+	protected final DataTypeImpl baseType;
+	
+	protected final int finalValue;
+	
+	public boolean isAtomType() { return baseType.isAtomType(); }
+	
+	public FinalComponent( DataTypeImpl baseType, int finalValue )
+	{
+		super( baseType.getName(), baseType.whiteSpace );
+		this.baseType = baseType;
+		this.finalValue = finalValue;
+	}
+	
+	public boolean isFinal( int derivationType )
+	{
+		if( (finalValue&derivationType) != 0 )	return true;
+		return baseType.isFinal(derivationType);
+	}
+	
+	public ConcreteType getConcreteType()
+	{
+		return baseType.getConcreteType();
+	}
+	public int isFacetApplicable( String facetName )
+	{
+		return baseType.isFacetApplicable(facetName);
+	}
+	public boolean checkFormat( String content, ValidationContextProvider context )
+	{
+		return baseType.checkFormat(content,context);
+	}
+	public Object convertToValue( String content, ValidationContextProvider context )
+	{
+		return baseType.convertToValue(content,context);
+	}
+	public DataTypeErrorDiagnosis diagnoseValue( String content, ValidationContextProvider context )
+	{
+		return baseType.diagnoseValue(content,context);
+	}
+	
+}

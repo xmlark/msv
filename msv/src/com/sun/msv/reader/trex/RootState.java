408355082ae7c2683cd43ced92cf4aa0cbf6664a
Bear <Bear@8cecbe3e-ef64-0410-a610-ece5afc49ac1>
2001-03-23 20:56:35 +0000

    support patterns whose document element is not "grammar".
    git-svn-id: https://svn.dev.java.net/svn/msv/trunk@193 8cecbe3e-ef64-0410-a610-ece5afc49ac1

--- msv/src/com/sun/msv/reader/trex/RootState.java
+++ msv/src/com/sun/msv/reader/trex/RootState.java
@@ -5,6 +5,7 @@ import com.sun.tranquilo.reader.State;
 import com.sun.tranquilo.reader.SimpleState;
 import com.sun.tranquilo.reader.ExpressionOwner;
 import com.sun.tranquilo.grammar.Expression;
+import com.sun.tranquilo.grammar.trex.TREXGrammar;
 
 /**
  * invokes State object that parses the document element.
@@ -15,12 +16,34 @@ class RootState extends SimpleState implements ExpressionOwner
 {
 	protected State createChildState( StartTagInfo tag )
 	{
+		// grammar has to be treated separately so as not to
+		// create unnecessary TREXGrammar object.
 		if(tag.localName.equals("grammar"))
 			return new GrammarState();
+		
+		State s = reader.createExpressionChildState(tag);
+		if(s!=null)
+		{// other pattern element is specified.
+			// create wrapper grammar
+			final TREXGrammarReader reader = (TREXGrammarReader)this.reader;
+			reader.grammar = new TREXGrammar( reader.getPool(), null );
+			simple = true;
+			return s;
+		}
+		
 		return null;
 	}
 	
+	/**
+	 * a flag that indicates 'grammar' element was not used.
+	 * In that case, this object is responsible to set start pattern.
+	 */
+	private boolean simple = false;
+	
 	// GrammarState implements ExpressionState,
 	// so RootState has to implement ExpressionOwner.
-	public void onEndChild(Expression exp) {}
+	public void onEndChild(Expression exp)
+	{
+		if( simple )	((TREXGrammarReader)reader).grammar.start = exp;
+	}
 }

1a973e40c95d89358fe4908c181cc3fba4e8864c
Bear <Bear@8cecbe3e-ef64-0410-a610-ece5afc49ac1>
2001-03-23 21:00:15 +0000

    modified to use stdout instead of stderr.
    git-svn-id: https://svn.dev.java.net/svn/msv/trunk@197 8cecbe3e-ef64-0410-a610-ece5afc49ac1

--- msv/src/com/sun/msv/driver/textui/DebugController.java
+++ msv/src/com/sun/msv/driver/textui/DebugController.java
@@ -9,30 +9,30 @@ public class DebugController implements GrammarReaderController
 {
 	public void warning( Locator[] loc, String errorMessage )
 	{
-		System.err.println(errorMessage);
+		System.out.println(errorMessage);
 	}
 	
 	public void error( Locator[] loc, String errorMessage, Exception nestedException )
 	{
 		if( nestedException instanceof SAXException )
 		{
-			System.err.println("SAXException");
+			System.out.println("SAXException");
 			nestedException.printStackTrace(System.err);
 			
 			SAXException se = (SAXException)nestedException;
 			if(se.getException()!=null)
 			{
-				System.err.println("nested exception");
+				System.out.println("nested exception");
 				se.getException().printStackTrace(System.err);
 			}
 		}
 		else
 		{
-			System.err.println(errorMessage);
+			System.out.println(errorMessage);
 		}
 		
 		for( int i=0; i<loc.length; i++ )
-			System.err.println( "  "+
+			System.out.println( "  "+
 				(loc[i].getLineNumber()+1)+":"+
 				loc[i].getColumnNumber()+"@"+
 				loc[i].getSystemId() );

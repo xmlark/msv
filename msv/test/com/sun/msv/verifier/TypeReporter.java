de9d57118234f1317e7802947cf53ce7e0c5df25
Bear <Bear@8cecbe3e-ef64-0410-a610-ece5afc49ac1>
2001-03-27 02:04:42 +0000

    Modified to support TREX type-assignment
    git-svn-id: https://svn.dev.java.net/svn/msv/trunk@211 8cecbe3e-ef64-0410-a610-ece5afc49ac1

--- msv/test/com/sun/msv/verifier/TypeReporter.java
+++ msv/test/com/sun/msv/verifier/TypeReporter.java
@@ -3,7 +3,9 @@ package com.sun.tranquilo.verifier;
 import com.sun.tranquilo.grammar.relax.ElementRule;
 import com.sun.tranquilo.grammar.relax.RELAXGrammar;
 import com.sun.tranquilo.grammar.trex.TREXPatternPool;
+import com.sun.tranquilo.grammar.trex.typed.TypedElementPattern;
 import com.sun.tranquilo.reader.relax.RELAXReader;
+import com.sun.tranquilo.reader.trex.typed.TypedTREXGrammarReader;
 import com.sun.tranquilo.verifier.VerifierFilter;
 import com.sun.tranquilo.verifier.regexp.trex.TREXDocumentDeclaration;
 import javax.xml.parsers.SAXParserFactory;
@@ -27,38 +29,53 @@ public class TypeReporter extends DefaultHandler
 	
 	private void run( String[] args ) throws Exception
 	{
-		if( args.length!=2 )
+		if( args.length!=3 )
 		{
-			System.out.println("Usage: TypeReporter <RELAX schema> <XML instance>\n");
+			System.out.println("Usage: TypeReporter (relax|trex) <schema> <XML instance>\n");
 			return;
 		}
 		
-		SAXParserFactory factory = new org.apache.crimson.jaxp.SAXParserFactoryImpl();
+//		SAXParserFactory factory = new org.apache.crimson.jaxp.SAXParserFactoryImpl();
+		SAXParserFactory factory = new org.apache.xerces.jaxp.SAXParserFactoryImpl();
 		factory.setNamespaceAware(true);
 		factory.setValidating(false);
-				
-		RELAXGrammar g =
-			RELAXReader.parse(
-				args[0],
-				factory,
-				new com.sun.tranquilo.driver.textui.DebugController(),
-				new TREXPatternPool() );
+		
+		TREXDocumentDeclaration docDecl;
+		
+		if( args[0].equals("relax") )
+		{
+			RELAXGrammar g =
+				RELAXReader.parse(
+					args[1],
+					factory,
+					new com.sun.tranquilo.driver.textui.DebugController(),
+					new TREXPatternPool() );
+			docDecl = new TREXDocumentDeclaration(
+				g.topLevel, (TREXPatternPool)g.pool, true );
+		}
+		else
+		{
+			docDecl = new TREXDocumentDeclaration(
+				TypedTREXGrammarReader.parse(
+					args[1],
+					factory,
+					new com.sun.tranquilo.driver.textui.DebugController() ) );
+		}
+		
 		// use TREXPatternPool so that we can verify it like TREX.
 		
-		if( g==null )
+		if( docDecl==null )
 		{
 			System.err.println("failed to load a grammar");
 			return;
 		}
 		
-		filter = new VerifierFilter(
-			new TREXDocumentDeclaration(
-			g.topLevel, (TREXPatternPool)g.pool, true ),
+		filter = new VerifierFilter( docDecl,
 			new com.sun.tranquilo.driver.textui.ReportErrorHandler() );
 		
 		filter.setParent(factory.newSAXParser().getXMLReader());
 		filter.setContentHandler(this);
-		filter.parse( args[1] );
+		filter.parse( args[2] );
 	}
 	
 	
@@ -78,16 +95,23 @@ public class TypeReporter extends DefaultHandler
 		System.out.print("<"+qName+"> :");
 		
 		Object o = filter.getVerifier().getCurrentElementType();
-		if(o==null)
-			System.out.println("???");
-		else
-		{
+		
+		if( o instanceof ElementRule )
+		{// for RELAX
 			ElementRule er = (ElementRule)o;
 			if( er.getParent()==null )
 				System.out.println("##inline");
 			else
 				System.out.println(er.getParent().name);
+			return;
 		}
+		if( o instanceof TypedElementPattern )
+		{// for TREX
+			System.out.println( ((TypedElementPattern)o).label );
+			return;
+		}
+		
+		System.out.println("???");
 	}
 	
 	public void endElement( String namespaceUri, String localName, String qName )

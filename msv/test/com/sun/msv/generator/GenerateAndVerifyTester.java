561b3b0880ddeb0f6e091c57f17189bc29fc1af1
Bear <Bear@8cecbe3e-ef64-0410-a610-ece5afc49ac1>
2001-04-02 23:21:05 +0000

    added a test case for generator
    git-svn-id: https://svn.dev.java.net/svn/msv/trunk@245 8cecbe3e-ef64-0410-a610-ece5afc49ac1

---
+++ msv/test/com/sun/msv/generator/GenerateAndVerifyTester.java
@@ -0,0 +1,12 @@
+package com.sun.tranquilo.generator;
+
+
+
+/**
+ * command line driver.
+ * 
+ * @author <a href="mailto:kohsuke.kawaguchi@eng.sun.com">Kohsuke KAWAGUCHI</a>
+ */
+public class GenerateAndVerifyTester extends Driver
+{
+}

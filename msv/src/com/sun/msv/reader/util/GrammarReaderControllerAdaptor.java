e5881dfbc886920744fb31a036eca11631dd5f39
Bear <Bear@8cecbe3e-ef64-0410-a610-ece5afc49ac1>
2001-04-17 23:00:04 +0000

    Moved from tranquilo/relaxns/reader
    git-svn-id: https://svn.dev.java.net/svn/msv/trunk@340 8cecbe3e-ef64-0410-a610-ece5afc49ac1

---
+++ msv/src/com/sun/msv/reader/util/GrammarReaderControllerAdaptor.java
@@ -0,0 +1,53 @@
+/*
+ * @(#)$Id$
+ *
+ * Copyright 2001 Sun Microsystems, Inc. All Rights Reserved.
+ * 
+ * This software is the proprietary information of Sun Microsystems, Inc.  
+ * Use is subject to license terms.
+ * 
+ */
+package com.sun.tranquilo.reader.util;
+
+import org.xml.sax.SAXParseException;
+import org.xml.sax.ErrorHandler;
+import org.xml.sax.Locator;
+import org.xml.sax.helpers.LocatorImpl;
+import com.sun.tranquilo.reader.GrammarReaderController;
+
+/**
+ * wraps GrammarReaderController by SAX ErrorHandler interface.
+ * 
+ * @author <a href="mailto:kohsuke.kawaguchi@eng.sun.com">Kohsuke KAWAGUCHI</a>
+ */
+public class GrammarReaderControllerAdaptor implements ErrorHandler
+{
+	private final GrammarReaderController core;
+	
+	public GrammarReaderControllerAdaptor( GrammarReaderController core ) {
+		this.core = core;
+	}
+	
+	public void fatalError( SAXParseException spe ) throws SAXParseException {
+		core.error( getLocator(spe), spe.getMessage(), spe.getException() );
+		throw spe;
+	}
+	
+	public void error( SAXParseException spe ) {
+		core.error( getLocator(spe), spe.getMessage(), spe.getException() );
+	}
+	
+	public void warning( SAXParseException spe ) {
+		core.warning( getLocator(spe), spe.getMessage() );
+	}
+			
+	protected Locator[] getLocator( SAXParseException spe ) {
+		LocatorImpl loc = new LocatorImpl();
+		loc.setColumnNumber( spe.getColumnNumber() );
+		loc.setLineNumber( spe.getLineNumber() );
+		loc.setSystemId( spe.getSystemId() );
+		loc.setPublicId( spe.getPublicId() );
+		
+		return new Locator[]{loc};
+	}
+}

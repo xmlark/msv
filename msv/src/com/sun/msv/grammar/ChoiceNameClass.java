7b37203c7ba03df223638f1c0cbe1f42504d4a48
Bear <Bear@8cecbe3e-ef64-0410-a610-ece5afc49ac1>
2001-04-24 19:47:47 +0000

    Modified to K&R style. Added toString method.
    git-svn-id: https://svn.dev.java.net/svn/msv/trunk@400 8cecbe3e-ef64-0410-a610-ece5afc49ac1

--- msv/src/com/sun/msv/grammar/ChoiceNameClass.java
+++ msv/src/com/sun/msv/grammar/ChoiceNameClass.java
@@ -14,22 +14,23 @@ package com.sun.tranquilo.grammar;
  * 
  * @author <a href="mailto:kohsuke.kawaguchi@eng.sun.com">Kohsuke KAWAGUCHI</a>
  */
-public class ChoiceNameClass implements NameClass
-{
+public class ChoiceNameClass implements NameClass {
 	public final NameClass nc1;
 	public final NameClass nc2;
 	
-	public boolean accepts( String namespaceURI, String localPart )
-	{
+	public boolean accepts( String namespaceURI, String localPart ) {
 		return nc1.accepts(namespaceURI,localPart)
 			|| nc2.accepts(namespaceURI,localPart);
 	}
 	
 	public Object visit( NameClassVisitor visitor ) { return visitor.onChoice(this); }
 	
-	public ChoiceNameClass( NameClass nc1, NameClass nc2 )
-	{
+	public ChoiceNameClass( NameClass nc1, NameClass nc2 ) {
 		this.nc1 = nc1;
 		this.nc2 = nc2;
+	}
+	
+	public String toString() {
+		return "%choice("+nc1+")";
 	}
 }

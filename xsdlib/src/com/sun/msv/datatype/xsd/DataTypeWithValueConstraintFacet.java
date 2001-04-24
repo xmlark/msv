fb5e524de534da68dffce9c9f5a14682f1db069f
Bear <Bear@8cecbe3e-ef64-0410-a610-ece5afc49ac1>
2001-04-24 18:20:32 +0000

    Modified to K&R style. Support checkFormat method. UnionType may call member's checkFormat methods. And we can checkFormat by converting to values.
    git-svn-id: https://svn.dev.java.net/svn/msv/trunk@390 8cecbe3e-ef64-0410-a610-ece5afc49ac1

--- xsdlib/src/com/sun/msv/datatype/xsd/DataTypeWithValueConstraintFacet.java
+++ xsdlib/src/com/sun/msv/datatype/xsd/DataTypeWithValueConstraintFacet.java
@@ -14,21 +14,18 @@ package com.sun.tranquilo.datatype;
  * 
  * @author	Kohsuke Kawaguchi
  */
-abstract class DataTypeWithValueConstraintFacet extends DataTypeWithFacet
-{
+abstract class DataTypeWithValueConstraintFacet extends DataTypeWithFacet {
+	
 	DataTypeWithValueConstraintFacet(
 		String typeName, DataTypeImpl baseType, String facetName, TypeIncubator facets )
-		throws BadTypeException
-	{
+		throws BadTypeException {
+	
 		super( typeName, baseType, facetName, facets );
 	}
 	
 	final protected boolean needValueCheck() { return true; }
 	
-	protected final boolean checkFormat( String literal, ValidationContextProvider context )
-	{
-		// since we always return true for needValueCheck,
-		// this method should never be called.
-		throw new IllegalStateException();
+	protected final boolean checkFormat( String literal, ValidationContextProvider context ) {
+		return convertToValue(literal,context)!=null;
 	}
 }

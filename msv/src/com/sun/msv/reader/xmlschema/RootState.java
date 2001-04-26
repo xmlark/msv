05fbcea2d320005e7d2923753da837b861fc81a5
Bear <Bear@8cecbe3e-ef64-0410-a610-ece5afc49ac1>
2001-04-26 18:56:01 +0000

    Modified to use RootIncludedState. This makes it much smaller.
    git-svn-id: https://svn.dev.java.net/svn/msv/trunk@428 8cecbe3e-ef64-0410-a610-ece5afc49ac1

--- msv/src/com/sun/msv/reader/xmlschema/RootState.java
+++ msv/src/com/sun/msv/reader/xmlschema/RootState.java
@@ -1,27 +1,17 @@
 package com.sun.tranquilo.reader.xmlschema;
 
-import com.sun.tranquilo.reader.SimpleState;
 import com.sun.tranquilo.reader.State;
-import com.sun.tranquilo.util.StartTagInfo;
 
 // used also for import.
-public class RootState extends SimpleState {
-	
-	protected State topLevelState;
+public class RootState extends RootIncludedSchemaState {
 	
 	protected RootState( State topLevelState ) {
-		this.topLevelState = topLevelState;
-	}
-	
-	protected State createChildState( StartTagInfo tag ) {
-		if(tag.localName.equals("schema"))
-			return topLevelState;
-		
-		return null;
+		super(topLevelState);
 	}
 	
 	protected void endSelf() {
 		final XMLSchemaReader reader = (XMLSchemaReader)this.reader;
+		// perform final wrap up.
 		reader.wrapUp();
 		super.endSelf();
 	}

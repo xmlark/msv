2e809d72140190b8c6627aae7436843435b936db
Bear <Bear@8cecbe3e-ef64-0410-a610-ece5afc49ac1>
2001-04-26 18:55:37 +0000

    Added documentation of how redefinition works. Fixed a bug in error recovery code that causes NullPointerException after "attempt to redefine undefined" error.
    git-svn-id: https://svn.dev.java.net/svn/msv/trunk@427 8cecbe3e-ef64-0410-a610-ece5afc49ac1

--- msv/src/com/sun/msv/reader/xmlschema/RedefinableDeclState.java
+++ msv/src/com/sun/msv/reader/xmlschema/RedefinableDeclState.java
@@ -4,10 +4,71 @@ import com.sun.tranquilo.grammar.ReferenceContainer;
 import com.sun.tranquilo.grammar.xmlschema.RedefinableExp;
 import com.sun.tranquilo.reader.ExpressionWithChildState;
 
-/** state that parses declaration.
+/** state that parses redefinable declaration.
  * 
  * "Declarations" are attribute, element, complexType, group, and attributeGroup.
  * simpleType is treated differently.
+ * 
+ * <p>
+ * When this state is used under states other than RedefineState, this class
+ * doesn't do anything. When used under RedefineState, this class does several
+ * tricks to make redefinition easy.
+ * 
+ * <p>
+ * Redefinition is done in the following steps.
+ * 
+ * <h2> Step.1 </h2>
+ * <p>
+ * First, say redefinition of declaration "ABC" is found, "ABC" has already
+ * defined once and it is
+ * 
+ * <ul>
+ *  <li>referenced from {@link ReferenceContainer} by name, and
+ *  <li>referenced from other expressions directly.
+ * </ul>
+ * 
+ * <img src="doc-files/redefine1.gif" />
+ * 
+ * <h2> Step.2 </h2>
+ * <p>
+ * In startSelf method, this class clones the current definition, and updates
+ * ReferenceContainer to point to the cloned definition (right side).
+ * 
+ * <img src="doc-files/redefine2.gif" />
+ * 
+ * <p>
+ * Note that other expressions hold direct reference to the original definition
+ * (left side), and these references are not affected by this update.
+ * 
+ * <h2> Step.3 </h2>
+ * <p>
+ * Body of redefinition is parsed and corresponding expression is created by
+ * derived class. This step is done no differently.
+ * 
+ * <p>
+ * Since ReferenceContainer has updated, any reference to this expression found
+ * during this step is bound to the cloned definition. This self reference usually
+ * happens.
+ * 
+ * <h2> Step.4 </h2>
+ * <p>
+ * After the body of redefinition is parsed, the original definition (left side)
+ * is updated by using new expression.
+ * 
+ * <img src="doc-files/redefine3.gif" />
+ * 
+ * <p>
+ * From now on, redefinition becomes visible to all expressions that hold
+ * direct reference to the original definition. The cloned definition is kept
+ * as-is so that any self reference found in the body will be maintained correctly.
+ * 
+ * <h2> Step.5 </h2>
+ * <p>
+ * Finally, ReferenceContainer is updated again to point to the updated definition.
+ * Therefore successive reference to "ABC" will be bound to the updated definition.
+ * Cloned old definition is kept as-is.
+ * 
+ * <img src="doc-files/redefine4.gif" />
  */
 public abstract class RedefinableDeclState extends ExpressionWithChildState {
 
@@ -45,6 +106,8 @@ public abstract class RedefinableDeclState extends ExpressionWithChildState {
 			oldDecl = (RedefinableExp)getContainer()._get(name);
 			if(oldDecl==null) {
 				reader.reportError( reader.ERR_REDEFINE_UNDEFINED, name );
+				// recover by creating a dummy object.
+				oldDecl = (RedefinableExp)getContainer()._getOrCreate(name);
 				return;
 			}
 			

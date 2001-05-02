9712f3649c8faf6a7ad4e91692630ea8a3f22d2a
Bear <Bear@8cecbe3e-ef64-0410-a610-ece5afc49ac1>
2001-05-02 20:58:50 +0000

    added a test case.
    git-svn-id: https://svn.dev.java.net/svn/msv/trunk@476 8cecbe3e-ef64-0410-a610-ece5afc49ac1

---
+++ msv/test/com/sun/msv/util/StringPairTest.java
@@ -0,0 +1,46 @@
+/*
+ * @(#)$Id$
+ *
+ * Copyright 2001 Sun Microsystems, Inc. All Rights Reserved.
+ * 
+ * This software is the proprietary information of Sun Microsystems, Inc.  
+ * Use is subject to license terms.
+ * 
+ */
+package com.sun.msv.util;
+
+import junit.framework.*;
+
+/**
+ * tests StringPair.
+ * 
+ * @author <a href="mailto:kohsuke.kawaguchi@eng.sun.com">Kohsuke KAWAGUCHI</a>
+ */
+public class StringPairTest extends TestCase {
+	
+	public StringPairTest( String name ) { super(name); }
+	
+	public static void main(java.lang.String[] args) {
+		junit.textui.TestRunner.run(suite());
+	}
+	
+	public static Test suite() {
+		return new TestSuite(StringPairTest.class);
+	}
+	
+	/** test equals and hashCode method */
+	public void testEqualsAndHashCode()	{
+		StringPair[] pairs = new StringPair[]{
+				new StringPair("namespace","foo"),
+				new StringPair("namespac-","foo"),
+				new StringPair("namespace","bar") };
+		
+		for( int i=0; i<pairs.length; i++ )
+			for( int j=0; j<pairs.length; j++ )
+				if( i==j ) {
+					assertEquals( pairs[i], pairs[j] );
+					assertEquals( pairs[i].hashCode(), pairs[j].hashCode() );
+				} else
+					assert( !pairs[i].equals(pairs[j]) );
+	}
+}

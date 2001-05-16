5f89d7c97f16a39e62d1a24187aa683dec6d623a
Bear <Bear@8cecbe3e-ef64-0410-a610-ece5afc49ac1>
2001-05-16 20:00:03 +0000

    implemented label and role attributes to inline element.
    git-svn-id: https://svn.dev.java.net/svn/msv/trunk@595 8cecbe3e-ef64-0410-a610-ece5afc49ac1

--- msv/src/com/sun/msv/reader/relax/core/InlineElementState.java
+++ msv/src/com/sun/msv/reader/relax/core/InlineElementState.java
@@ -16,6 +16,7 @@ import com.sun.msv.grammar.Expression;
 import com.sun.msv.grammar.SimpleNameClass;
 import com.sun.msv.grammar.relax.ElementRule;
 import com.sun.msv.grammar.relax.TagClause;
+import com.sun.msv.grammar.relax.AttPoolClause;
 import com.sun.msv.reader.State;
 import com.sun.msv.reader.ExpressionState;
 import com.sun.msv.reader.datatype.xsd.FacetStateParent;
@@ -26,36 +27,47 @@ import com.sun.msv.util.StartTagInfo;
  * 
  * @author <a href="mailto:kohsuke.kawaguchi@eng.sun.com">Kohsuke KAWAGUCHI</a>
  */
-public class InlineElementState extends ExpressionState implements FacetStateParent
-{
+public class InlineElementState extends ExpressionState implements FacetStateParent {
+	
+	/** this field is set to null if this element has label attribute. */
 	protected TypeIncubator incubator;
 	
 	public TypeIncubator getIncubator() { return incubator; }
 	
 	protected State createChildState( StartTagInfo tag ) {
-		return ((RELAXCoreReader)reader).createFacetState(this,tag);	// facets
+		if( incubator!=null )
+			return ((RELAXCoreReader)reader).createFacetState(this,tag);	// facets
+		else
+			return null;	// nothing is allowed when @label is used.
 	}
 	
-	protected void startSelf()
-	{
+	protected void startSelf() {
 		super.startSelf();
 		String type		= startTag.getAttribute("type");
-		if(type==null)
-		{
-			reader.reportError( RELAXCoreReader.ERR_MISSING_ATTRIBUTE, "element", "type" );
+		String label	= startTag.getAttribute("label");
+		
+		if( type!=null && label!=null )
+			reader.reportError( reader.ERR_CONFLICTING_ATTRIBUTES, "type", "label" );
+			// recover by ignoring one attribute.
+		
+		if( type==null && label==null ) {
+			reader.reportError( reader.ERR_MISSING_ATTRIBUTE_2, "element", "type", "label" );
 			type="string";
 		}
 		
-		incubator = new TypeIncubator( reader.resolveDataType(type) );
+		if( label!=null ) {
+			incubator = null;
+		} else {
+			incubator = new TypeIncubator( reader.resolveDataType(type) );
+		}
 	}
-	protected Expression makeExpression()
-	{
-		try
-		{
+	
+	protected Expression makeExpression() {
+		try {
+			final RELAXCoreReader reader = (RELAXCoreReader)this.reader;
 			final String name		= startTag.getAttribute("name");
 			
-			if( name==null )
-			{
+			if( name==null ) {
 				reader.reportError( reader.ERR_MISSING_ATTRIBUTE, "element","name" );
 				// recover by ignoring this element.
 				return Expression.nullSet;
@@ -63,26 +75,41 @@ public class InlineElementState extends ExpressionState implements FacetStatePar
 			
 			Expression contentModel;
 
-			if( !startTag.containsAttribute("type") && incubator.isEmpty() )
-			{// we can use cheaper anyString
-				contentModel = Expression.anyString;
-			}
-			else
-			{
-				contentModel = reader.pool.createTypedString(
-					incubator.derive(null) );
+			if( incubator!=null ) {
+				// @type is used
+				if( startTag.containsAttribute("type") && incubator.isEmpty() ) {
+					// we can use cheaper anyString
+					contentModel = Expression.anyString;
+				} else {
+					contentModel = reader.pool.createTypedString(
+						incubator.derive(null) );
+				}
+			} else {
+				// @label is used
+				String label = startTag.getAttribute("label");
+				if(label==null)	throw new Error();
+				
+				contentModel = reader.module.hedgeRules.getOrCreate(label);
+				reader.backwardReference.memorizeLink(contentModel);
 			}
 			
 			TagClause c = new TagClause();
 			c.nameClass = new SimpleNameClass( ((RELAXCoreReader)reader).module.targetNamespace, name );
-			c.exp		= Expression.epsilon;	// no attribute
+			
+			final String role = startTag.getAttribute("role");
+			if( role==null )	c.exp = Expression.epsilon;	// no attribute
+			else {
+				// role attribute
+				AttPoolClause att = reader.module.attPools.getOrCreate(role);
+				c.exp = att;
+				reader.backwardReference.memorizeLink(att);
+			}
 			
 			// create anonymous ElementRule. this rule will never be added to
 			// RefContainer.
 			return new ElementRule( reader.pool, c, contentModel );
-		}
-		catch( BadTypeException e )
-		{// derivation failed
+		} catch( BadTypeException e ) {
+			// derivation failed
 			reader.reportError( e, reader.ERR_BAD_TYPE, e.getMessage() );
 			// recover by using harmless expression. anything will do.
 			return Expression.nullSet;

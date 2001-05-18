caef5ef81a64bbaa5b25231fafc86914872defd7
Bear <Bear@8cecbe3e-ef64-0410-a610-ece5afc49ac1>
2001-05-18 19:45:35 +0000

    Moved from TREX reader.
    git-svn-id: https://svn.dev.java.net/svn/msv/trunk@608 8cecbe3e-ef64-0410-a610-ece5afc49ac1

---
+++ msv/src/com/sun/msv/reader/InterleaveState.java
@@ -0,0 +1,27 @@
+/*
+ * @(#)$Id$
+ *
+ * Copyright 2001 Sun Microsystems, Inc. All Rights Reserved.
+ * 
+ * This software is the proprietary information of Sun Microsystems, Inc.  
+ * Use is subject to license terms.
+ * 
+ */
+package com.sun.msv.reader;
+
+import com.sun.msv.grammar.Expression;
+import com.sun.msv.reader.ExpressionWithChildState;
+
+/**
+ * state that creates an InterleaveExp.
+ * 
+ * @author <a href="mailto:kohsuke.kawaguchi@eng.sun.com">Kohsuke KAWAGUCHI</a>
+ */
+public class InterleaveState extends ExpressionWithChildState {
+	
+	protected Expression castExpression( Expression exp, Expression child ) {
+		// first one.
+		if( exp==null )		return child;
+		return reader.pool.createInterleave(exp,child);
+	}
+}

48cb74be1eb04b49469febcb81e5612936f391a4
Bear <Bear@8cecbe3e-ef64-0410-a610-ece5afc49ac1>
2001-05-29 22:47:30 +0000

    TREX extensions are merged to the main primitive set.
    git-svn-id: https://svn.dev.java.net/svn/msv/trunk@628 8cecbe3e-ef64-0410-a610-ece5afc49ac1

---
+++ msv/src/com/sun/msv/grammar/ConcurExp.java
@@ -0,0 +1,43 @@
+/*
+ * @(#)$Id$
+ *
+ * Copyright 2001 Sun Microsystems, Inc. All Rights Reserved.
+ * 
+ * This software is the proprietary information of Sun Microsystems, Inc.  
+ * Use is subject to license terms.
+ * 
+ */
+package com.sun.msv.grammar;
+
+/**
+ * &lt;concur&gt; pattern of TREX.
+ * 
+ * No of the other language needs this expression.
+ * So application can deny processing this expression if neceesary.
+ * 
+ * @author <a href="mailto:kohsuke.kawaguchi@eng.sun.com">Kohsuke KAWAGUCHI</a>
+ */
+public final class ConcurExp extends BinaryExp {
+	
+	ConcurExp( Expression left, Expression right ) { super(left,right,HASHCODE_CONCUR); }
+
+	public Object visit( ExpressionVisitor visitor ) {
+		return visitor.onConcur(this);
+	}
+
+	public Expression visit( ExpressionVisitorExpression visitor ) {
+		return visitor.onConcur(this);
+	}
+	
+	public boolean visit( ExpressionVisitorBoolean visitor ) {
+		return visitor.onConcur(this);
+	}
+
+	public void visit( ExpressionVisitorVoid visitor ) {
+		visitor.onConcur(this);
+	}
+
+	protected boolean calcEpsilonReducibility() {
+		return exp1.isEpsilonReducible() && exp2.isEpsilonReducible();
+	}
+}

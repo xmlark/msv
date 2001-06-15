f988472477a15bbb1d76aaef56cbeadca6cbb4f6
Bear <Bear@8cecbe3e-ef64-0410-a610-ece5afc49ac1>
2001-06-15 02:06:26 +0000

    modified the coding style.
    git-svn-id: https://svn.dev.java.net/svn/msv/trunk@773 8cecbe3e-ef64-0410-a610-ece5afc49ac1

--- msv/src/com/sun/msv/verifier/regexp/ResidualCalculator.java
+++ msv/src/com/sun/msv/verifier/regexp/ResidualCalculator.java
@@ -19,23 +19,20 @@ import com.sun.msv.grammar.*;
  * 
  * @author <a href="mailto:kohsuke.kawaguchi@eng.sun.com">Kohsuke KAWAGUCHI</a>
  */
-public class ResidualCalculator implements ExpressionVisitorExpression
-{
+public class ResidualCalculator implements ExpressionVisitorExpression {
 	protected Token token;
 	protected final ExpressionPool pool;
 	
 	protected ResidualCalculator( ExpressionPool pool )	{ this.pool=pool; }
 	
 	/** compute the residual */
-	final Expression calcResidual( Expression exp, ElementToken token )
-	{
-		if( token.acceptedPatterns!=null && token.acceptedPatterns.length==1 )
-		{// we can use optimization table
+	final Expression calcResidual( Expression exp, ElementToken token ) {
+		if( token.acceptedPatterns!=null && token.acceptedPatterns.length==1 ) {
+			// we can use optimization table
 			OptimizationTag ot;
 			if(exp.verifierTag==null)
 				exp.verifierTag = ot = new OptimizationTag();
-			else
-			{
+			else {
 				ot = (OptimizationTag)exp.verifierTag;
 				Expression residual = (Expression)ot.simpleElementTokenResidual.get(token.acceptedPatterns[0]);
 				if(residual!=null)
@@ -55,8 +52,7 @@ public class ResidualCalculator implements ExpressionVisitorExpression
 	}
 	
 	/** compute the residual */
-	public final Expression calcResidual( Expression exp, Token token )
-	{
+	public final Expression calcResidual( Expression exp, Token token ) {
 		if( token instanceof ElementToken )
 			return calcResidual(exp,(ElementToken)token);
 		
@@ -64,55 +60,45 @@ public class ResidualCalculator implements ExpressionVisitorExpression
 		return exp.visit(this);
 	}
 	
-	public Expression onAttribute( AttributeExp exp )
-	{
+	public Expression onAttribute( AttributeExp exp ) {
 		if(token.match(exp))	return Expression.epsilon;
 		else				return Expression.nullSet;
 	}
-	public Expression onChoice( ChoiceExp exp )
-	{
+	public Expression onChoice( ChoiceExp exp ) {
 		return pool.createChoice( exp.exp1.visit(this), exp.exp2.visit(this) );
 	}
-	public Expression onElement( ElementExp exp )
-	{
+	public Expression onElement( ElementExp exp ) {
 		if(token.match(exp))	return Expression.epsilon;
 		else				return Expression.nullSet;
 	}
-	public Expression onOneOrMore( OneOrMoreExp exp )
-	{
+	public Expression onOneOrMore( OneOrMoreExp exp ) {
 		return pool.createSequence(
 			exp.exp.visit(this),
 			pool.createZeroOrMore(exp.exp) );
 	}
-	public Expression onMixed( MixedExp exp )
-	{
+	public Expression onMixed( MixedExp exp ) {
 		// if token can be interpreted as anyString, eat it.
 		if( token.matchAnyString() )	return exp;
 		
 		// otherwise, it must be consumed by the children.
 		return pool.createMixed( exp.exp.visit(this) );
 	}
-	public Expression onEpsilon()
-	{
+	public Expression onEpsilon() {
 		return Expression.nullSet;
 	}
-	public Expression onNullSet()
-	{
+	public Expression onNullSet() {
 		return Expression.nullSet;
 	}
-	public Expression onAnyString()
-	{
+	public Expression onAnyString() {
 		// anyString is not reduced to epsilon.
 		// it remains there even after consuming StringToken.
 		if(token.matchAnyString())		return Expression.anyString;
 		else							return Expression.nullSet;
 	}
-	public Expression onRef( ReferenceExp exp )
-	{
+	public Expression onRef( ReferenceExp exp ) {
 		return exp.exp.visit(this);
 	}
-	public Expression onSequence( SequenceExp exp )
-	{
+	public Expression onSequence( SequenceExp exp ) {
 		Expression r = pool.createSequence( exp.exp1.visit(this), exp.exp2 );
 		
 		if( exp.exp1.isEpsilonReducible() )
@@ -135,8 +121,7 @@ public class ResidualCalculator implements ExpressionVisitorExpression
 		return pool.createConcur(
 			exp.exp1.visit(this), exp.exp2.visit(this) );
 	}
-	public Expression onInterleave( InterleaveExp exp )
-	{
+	public Expression onInterleave( InterleaveExp exp ) {
 		return pool.createChoice(
 			pool.createInterleave( exp.exp1.visit(this), exp.exp2 ),
 			pool.createInterleave( exp.exp1, exp.exp2.visit(this) ) );

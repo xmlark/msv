d26bfd163cf34242bc4af9ccb5b34325855988cf
Bear <Bear@8cecbe3e-ef64-0410-a610-ece5afc49ac1>
2001-06-15 02:07:59 +0000

    added documentation.
    git-svn-id: https://svn.dev.java.net/svn/msv/trunk@777 8cecbe3e-ef64-0410-a610-ece5afc49ac1

--- msv/src/com/sun/msv/util/DatatypeRef.java
+++ msv/src/com/sun/msv/util/DatatypeRef.java
@@ -15,5 +15,8 @@ package com.sun.msv.util;
  * @author <a href="mailto:kohsuke.kawaguchi@eng.sun.com">Kohsuke KAWAGUCHI</a>
  */
 public class DataTypeRef {
+	/**
+	 * if the size of the array is zero, then that means this token is ignored.
+	 */
 	public org.relaxng.datatype.DataType[] types = null;
 }

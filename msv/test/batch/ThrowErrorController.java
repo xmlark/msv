166e45b87f0dc4b206e8dd2ae6dc105251e1b66e
Bear <Bear@8cecbe3e-ef64-0410-a610-ece5afc49ac1>
2001-06-21 16:41:12 +0000

    Modified to dump the nested exception if available.
    git-svn-id: https://svn.dev.java.net/svn/msv/trunk@799 8cecbe3e-ef64-0410-a610-ece5afc49ac1

--- msv/test/batch/ThrowErrorController.java
+++ msv/test/batch/ThrowErrorController.java
@@ -12,6 +12,7 @@ package batch;
 import com.sun.msv.reader.GrammarReaderController;
 import org.xml.sax.InputSource;
 import org.xml.sax.Locator;
+import org.xml.sax.SAXException;
 
 /**
  * GrammarReaderController implementation that throws an Error
@@ -21,11 +22,14 @@ import org.xml.sax.Locator;
  * 
  * @author <a href="mailto:kohsuke.kawaguchi@eng.sun.com">Kohsuke KAWAGUCHI</a>
  */
-public class ThrowErrorController implements GrammarReaderController
-{
+public class ThrowErrorController implements GrammarReaderController {
+	
 	public InputSource resolveEntity(String s,String r){return null;}
-	public void error( Locator[] locs, String msg, Exception nested )
-	{
+	public void error( Locator[] locs, String msg, Exception nested ) {
+		if( nested instanceof SAXException ) {
+			if(((SAXException)nested).getException()!=null)
+				((SAXException)nested).getException().printStackTrace();
+		}
 		throw new Error(msg);
 	}
 	public void warning( Locator[] locs, String msg ) {}

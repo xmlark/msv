90d25594f727f3976b219c33f957fc787a7f6ae5
Bear <Bear@8cecbe3e-ef64-0410-a610-ece5afc49ac1>
2001-06-29 19:20:58 +0000

    Implemented more feature to support various grammars.
    git-svn-id: https://svn.dev.java.net/svn/msv/trunk@855 8cecbe3e-ef64-0410-a610-ece5afc49ac1

--- tahiti/test/TestDriver.java
+++ tahiti/test/TestDriver.java
@@ -7,25 +7,32 @@ public class TestDriver
 {
 	public static void main( String args[] ) throws Exception {
 		
+		if( args.length!=2 ) {
+			System.out.println(
+				"Usage: TestDriver <grammar class name> <XML file name>\n"+
+				"  This test driver uses the specified grammar to \n"+
+				"  parse the specified instance.");
+			return;
+		}
 		SAXParserFactory factory = new org.apache.xerces.jaxp.SAXParserFactoryImpl();
 		factory.setNamespaceAware(true);
 
 		System.out.println("preparing a grammar");
 		long cnt = System.currentTimeMillis();
 		
-		Unmarshaller unmarshaller = new Unmarshaller( getGrammar() );
+		Unmarshaller unmarshaller = new Unmarshaller( getGrammar(args[0]) );
 		
 		System.out.println("preparation complete: ("+(System.currentTimeMillis()-cnt)+"ms)");
 		
 		XMLReader reader = factory.newSAXParser().getXMLReader();
 		reader.setContentHandler(unmarshaller);
 		
-		reader.parse(args[0]);
+		reader.parse(args[1]);
 		
 		System.out.println(unmarshaller.getResult());
 	}
 	
-	static BindableGrammar getGrammar() {
-		return out.sub.Name.grammar;
+	static BindableGrammar getGrammar( String className ) throws Exception {
+		return (BindableGrammar)Class.forName(className).getField("grammar").get(null);
 	}
 }

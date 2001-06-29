ac74090b24b0dc43e0e9449e1e46803d568e2648
Bear <Bear@8cecbe3e-ef64-0410-a610-ece5afc49ac1>
2001-06-29 02:50:42 +0000

    Added two more utility classes.
    git-svn-id: https://svn.dev.java.net/svn/msv/trunk@845 8cecbe3e-ef64-0410-a610-ece5afc49ac1

---
+++ msv/src/com/sun/msv/grammar/util/NameClassSimplifier.java
@@ -0,0 +1,104 @@
+/*
+ * @(#)$Id$
+ *
+ * Copyright 2001 Sun Microsystems, Inc. All Rights Reserved.
+ * 
+ * This software is the proprietary information of Sun Microsystems, Inc.  
+ * Use is subject to license terms.
+ * 
+ */
+package com.sun.msv.grammar.util;
+
+import com.sun.msv.grammar.*;
+import com.sun.msv.util.StringPair;
+import java.util.Set;
+import java.util.Iterator;
+
+/**
+ * minimizes a name class.
+ * 
+ * Sometimes, a name class could become unnecessarily big. For example,
+ * 
+ * <PRE><XMP>
+ * <choice>
+ *   <anyName/>
+ *   <anyName/>
+ *   <anyName/>
+ * </choice>
+ * </XMP></PRE>
+ * 
+ * This procedure converts those name classes to the equivalent small name class.
+ * 
+ * @author <a href="mailto:kohsuke.kawaguchi@eng.sun.com">Kohsuke KAWAGUCHI</a>
+ */
+public class NameClassSimplifier {
+	
+	public static NameClass simplify( NameClass nc ) {
+		final Set possibleNames = PossibleNamesCollector.calc(nc);
+		final String MAGIC = PossibleNamesCollector.MAGIC;
+		
+		Set uris = new java.util.HashSet();
+		
+		Iterator itr = possibleNames.iterator();
+		while( itr.hasNext() ) {
+			StringPair name = (StringPair)itr.next();
+			if( name.localName!=MAGIC ) {
+				// a simple name.
+				if( nc.accepts(name)==nc.accepts( name.namespaceURI, MAGIC ) ) {
+					itr.remove();
+					continue;
+				}
+			} else
+			if( name.namespaceURI!=MAGIC ) {
+				// a ns name
+				if( nc.accepts(name)==nc.accepts(MAGIC,MAGIC) ) {
+					itr.remove();
+					continue;
+				}
+			}
+			
+			// collect the remainig namespace URIs.
+			if( name.namespaceURI!=MAGIC )
+				uris.add(name.namespaceURI);
+		}
+		
+		if( !nc.accepts(MAGIC,MAGIC) )
+			possibleNames.remove( new StringPair(MAGIC,MAGIC) );
+		
+		NameClass result = null;
+		Iterator jtr = uris.iterator();
+		while( jtr.hasNext() ) {
+			final String uri = (String)jtr.next();
+			
+			NameClass local = null;
+			itr = possibleNames.iterator();
+			while( itr.hasNext() ) {
+				final StringPair name = (StringPair)itr.next();
+				
+				if(!name.namespaceURI.equals(uri))		continue;
+				if(name.localName==MAGIC)				continue;
+				
+				if(local==null)	local = new SimpleNameClass(name);
+				else			local = new ChoiceNameClass(local,new SimpleNameClass(name));
+			}
+			if(possibleNames.contains(new StringPair(uri,MAGIC))) {
+				if(local==null)
+					local = new NamespaceNameClass(uri);
+				else
+					local = new DifferenceNameClass(new NamespaceNameClass(uri),local);
+			}
+			
+			if(local!=null) {
+				if(result==null)	result = local;
+				else				result = new ChoiceNameClass(result,local);
+			}
+		}
+		
+		if( nc.accepts(MAGIC,MAGIC) ) {
+			if(result==null)		result = AnyNameClass.theInstance;
+			else					result = new NotNameClass(result);
+		}
+		
+		return result;
+	}
+}
---
+++ msv/src/com/sun/msv/grammar/util/PossibleNamesCollector.java
@@ -0,0 +1,71 @@
+/*
+ * @(#)$Id$
+ *
+ * Copyright 2001 Sun Microsystems, Inc. All Rights Reserved.
+ * 
+ * This software is the proprietary information of Sun Microsystems, Inc.  
+ * Use is subject to license terms.
+ * 
+ */
+package com.sun.msv.grammar.util;
+
+import com.sun.msv.grammar.*;
+import com.sun.msv.util.StringPair;
+import java.util.Set;
+
+/**
+ * computes the possible names.
+ * 
+ * <p>
+ * See <a href="http://lists.oasis-open.org/ob/htsearch?config=lists_oasis-open_org&restrict=relax-ng%2F&method=and&sort=score&words=possibleNames">
+ * the description</a>.
+ * 
+ * @author <a href="mailto:kohsuke.kawaguchi@sun.com">Kohsuke KAWAGUCHI</a>
+ */
+public class PossibleNamesCollector implements NameClassVisitor {
+	
+	/**
+	 * computes all possibile names for this name class, and returns
+	 * the set of {@link StringPair}.
+	 */
+	public static Set calc( NameClass nc ) {
+		PossibleNamesCollector col = new PossibleNamesCollector();
+		nc.visit(col);
+		return col.names;
+	}
+	
+	
+	public static final String MAGIC = "\u0000";
+	private static final StringPair pairForAny = new StringPair( MAGIC, MAGIC );
+	
+	/** this set will receive all possible names. */
+	private Set names = new java.util.HashSet();
+	
+	public Object onChoice( ChoiceNameClass nc ) {
+		nc.nc1.visit(this);
+		nc.nc2.visit(this);
+		return null;
+	}
+	public Object onAnyName( AnyNameClass nc ) {
+		names.add( pairForAny );
+		return null;
+	}
+	public Object onSimple( SimpleNameClass nc ) {
+		names.add( new StringPair( nc.namespaceURI, nc.localName ) );
+		return null;
+	}
+	public Object onNsName( NamespaceNameClass nc ) {
+		names.add( new StringPair( nc.namespaceURI, MAGIC ) );
+		return null;
+	}
+	public Object onNot( NotNameClass nc ) {
+		names.add( pairForAny );
+		nc.child.visit(this);
+		return null;
+	}
+	public Object onDifference( DifferenceNameClass nc ) {
+		nc.nc1.visit(this);
+		nc.nc2.visit(this);
+		return null;
+	}
+};

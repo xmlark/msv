0c2c4ad4bc90f2497b17dab4181ebfb484f13b2d
Bear <Bear@8cecbe3e-ef64-0410-a610-ece5afc49ac1>
2001-07-14 00:32:46 +0000

    added a new unmarshall method.
    git-svn-id: https://svn.dev.java.net/svn/msv/trunk@878 8cecbe3e-ef64-0410-a610-ece5afc49ac1

--- tahiti/src/com/sun/tahiti/runtime/ll/Unmarshaller.java
+++ tahiti/src/com/sun/tahiti/runtime/ll/Unmarshaller.java
@@ -12,6 +12,12 @@ package com.sun.tahiti.runtime.ll;
 import com.sun.tahiti.runtime.TypeDetecter;
 import com.sun.msv.grammar.Grammar;
 import com.sun.msv.verifier.regexp.REDocumentDeclaration;
+import javax.xml.parsers.SAXParserFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import org.xml.sax.XMLReader;
+import org.xml.sax.SAXException;
+import org.xml.sax.InputSource;
+import java.io.IOException;
 
 public class Unmarshaller extends TypeDetecter {
 	
@@ -25,5 +31,40 @@ public class Unmarshaller extends TypeDetecter {
 	
 	public Object getResult() {
 		return binder.getResult();
+	}
+	
+	/**
+	 * reads an XML document from the specified location and returns
+	 * the parsed object.
+	 * 
+	 * <p>
+	 * This method will use the default XML parser to parse the document.
+	 */
+	public static Object unmarshall( BindableGrammar grammar, String xmlDocument )
+		throws IOException, SAXException, ParserConfigurationException {
+		
+		return unmarshall( grammar, new InputSource(xmlDocument) );
+	}
+	
+	/**
+	 * reads an XML document from the specified InputSource and returns
+	 * the parsed object.
+	 * 
+	 * <p>
+	 * This method will use the default XML parser to parse the document.
+	 */
+	public static Object unmarshall( BindableGrammar grammar, InputSource xmlDocument )
+		throws IOException, SAXException, ParserConfigurationException {
+
+		SAXParserFactory factory = SAXParserFactory.newInstance();
+		factory.setNamespaceAware(true);
+		XMLReader reader = factory.newSAXParser().getXMLReader();
+
+		Unmarshaller unmarshaller = new Unmarshaller(grammar);
+		reader.setContentHandler(unmarshaller);
+		
+		reader.parse(xmlDocument);
+		
+		return unmarshaller.getResult();
 	}
 }

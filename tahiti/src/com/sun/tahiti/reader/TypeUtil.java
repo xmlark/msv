6002b56d58aeb3acd03e6f9bf6896e645ed1d0a3
Bear <Bear@8cecbe3e-ef64-0410-a610-ece5afc49ac1>
2001-07-14 02:10:10 +0000

    The getCommonBaseType method is moved to its own class.
    git-svn-id: https://svn.dev.java.net/svn/msv/trunk@882 8cecbe3e-ef64-0410-a610-ece5afc49ac1

---
+++ tahiti/src/com/sun/tahiti/reader/TypeUtil.java
@@ -0,0 +1,34 @@
+package com.sun.tahiti.reader;
+
+import com.sun.tahiti.grammar.*;
+import java.util.Set;
+
+public class TypeUtil {
+	
+	/**
+	 * compute the common base type of types.
+	 * 
+	 * TODO: this is a very interesting problem. Since one type has possibly
+	 * multiple base types, it's not an easy problem.
+	 * The current implementation is very naive.
+	 */
+	public static Type getCommonBaseType( Type[] t ) {
+		// TODO:
+		
+		for( int i=1; i<t.length; i++ )
+			if(t[0]!=t[i])
+				return SystemType.get(Object.class);
+		
+		return t[0];
+	}
+	
+	/**
+	 * compute the common base type of two types.
+	 * 
+	 * @param types
+	 *		set of {@link Type} objects.
+	 */
+	public static Type getCommonBaseType( Set types ) {
+		return getCommonBaseType( (Type[])types.toArray(new Type[types.size()]) );
+	}
+}

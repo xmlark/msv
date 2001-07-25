a609ceee7432944ec7815ae3f22d153a72787652
Bear <Bear@8cecbe3e-ef64-0410-a610-ece5afc49ac1>
2001-07-25 20:56:59 +0000

    Added for a performance reason.
    git-svn-id: https://svn.dev.java.net/svn/msv/trunk@963 8cecbe3e-ef64-0410-a610-ece5afc49ac1

---
+++ msv/src/com/sun/msv/util/LightStack.java
@@ -0,0 +1,38 @@
+/*
+ * @(#)$Id$
+ *
+ * Copyright 2001 Sun Microsystems, Inc. All Rights Reserved.
+ * 
+ * This software is the proprietary information of Sun Microsystems, Inc.  
+ * Use is subject to license terms.
+ * 
+ */
+package com.sun.msv.util;
+
+/**
+ * light-weight stack implementation.
+ * 
+ * This one is unsynchronized, and never shrink its memory footprint, but fast.
+ * 
+ * @author <a href="mailto:kohsuke.kawaguchi@sun.com">Kohsuke KAWAGUCHI</a>
+ */
+public final class LightStack {
+	
+	private Object[] buf = new Object[8];
+	private int len = 0;
+	
+	public void push( Object o ) {
+		try {
+			buf[len++] = o;
+		} catch( ArrayIndexOutOfBoundsException e ) {
+			Object[] nbuf = new Object[buf.length*2];
+			System.arraycopy( buf, 0, nbuf, 0, buf.length );
+			buf = nbuf;
+			buf[len++] = o;
+		}
+	}
+	
+	public Object pop() {
+		return buf[--len];
+	}
+}

badfc617f65ff88e0344ffbbaffa8c1129f666b5
Bear <Bear@8cecbe3e-ef64-0410-a610-ece5afc49ac1>
2001-08-02 17:31:54 +0000

    verify(Node) method is implemented.
    git-svn-id: https://svn.dev.java.net/svn/msv/trunk@1010 8cecbe3e-ef64-0410-a610-ece5afc49ac1

--- msv/src/com/sun/msv/verifier/jarv/VerifierImpl.java
+++ msv/src/com/sun/msv/verifier/jarv/VerifierImpl.java
@@ -17,9 +17,11 @@ import org.xml.sax.InputSource;
 import org.xml.sax.XMLReader;
 import org.xml.sax.SAXException;
 import org.w3c.dom.Node;
+import org.w3c.dom.Document;
 import com.sun.msv.verifier.DocumentDeclaration;
 import com.sun.msv.verifier.ValidityViolation;
 import com.sun.msv.verifier.util.VerificationErrorHandlerImpl;
+import com.sun.msv.util.xml.SAXEventGenerator;
 import java.io.IOException;
 
 /**
@@ -32,93 +34,78 @@ class VerifierImpl implements Verifier
 	private final DocumentDeclaration grammar;
 	private final XMLReader reader;
 	
-	VerifierImpl( DocumentDeclaration grammar, XMLReader reader )
-	{
+	VerifierImpl( DocumentDeclaration grammar, XMLReader reader ) {
 		this.grammar = grammar;
 		this.reader	= reader;
 	}
 	
-	public boolean isFeature(String featureName)
-		throws SAXNotRecognizedException
-	{
+	public boolean isFeature(String featureName) throws SAXNotRecognizedException {
 		if( featureName.equals(FEATURE_HANDLER) )	return true;
 		if( featureName.equals(FEATURE_FILTER) )	return true;
 
 		throw new SAXNotRecognizedException(featureName);
 	}
 	
-	public void setFeature( String feature, boolean value )
-		throws SAXNotRecognizedException
-	{
+	public void setFeature( String feature, boolean value ) throws SAXNotRecognizedException {
 		throw new SAXNotRecognizedException(feature);
 	}
 	
 	public Object getProperty( String property )
-		throws SAXNotRecognizedException
-	{
+				throws SAXNotRecognizedException {
 		throw new SAXNotRecognizedException(property);
 	}
 	
 	public void setProperty( String property, Object value )
-		throws SAXNotRecognizedException
-	{
+				throws SAXNotRecognizedException {
 		throw new SAXNotRecognizedException(property);
 	}
 	
-	public void setErrorHandler( ErrorHandler handler )
-	{
+	public void setErrorHandler( ErrorHandler handler ) {
 		reader.setErrorHandler(handler);
 	}
 	
-	public void setEntityResolver( EntityResolver handler )
-	{
+	public void setEntityResolver( EntityResolver handler ) {
 		reader.setEntityResolver(handler);
 	}
 
     public boolean verify(String uri)
-		throws SAXException, IOException
-	{
+			throws SAXException, IOException {
 		reader.setContentHandler(getVerifierHandler());
-		try
-		{
+		try {
 			reader.parse(uri);
 			return true;
-		}
-		catch(ValidityViolation v)
-		{
+		} catch(ValidityViolation v) {
 			return false;
 		}
 	}
 
-    public boolean verify(InputSource source)
-		throws SAXException, IOException
-	{
+    public boolean verify(InputSource source) throws SAXException, IOException {
 		reader.setContentHandler(getVerifierHandler());
-		try
-		{
+		try {
 			reader.parse(source);
 			return true;
-		}
-		catch(ValidityViolation v)
-		{
+		} catch(ValidityViolation v) {
 			return false;
 		}
 	}
 
-	public boolean verify(Node node)
-	{
-		throw new UnsupportedOperationException();
+	public boolean verify(Node node) throws SAXException {
+		if(!(node instanceof Document))
+			throw new UnsupportedOperationException("nothing but Document is supported");
+		
+		try {
+			SAXEventGenerator.parse((Document)node,getVerifierHandler());
+			return true;
+		} catch(ValidityViolation v) {
+			return false;
+		}
 	}
 
-    public VerifierHandler getVerifierHandler()
-	{
-		return new HandlerImpl( grammar,
-			new VerificationErrorHandlerImpl() );
+    public VerifierHandler getVerifierHandler() {
+		return new HandlerImpl( grammar, new VerificationErrorHandlerImpl() );
     }
 
-    public VerifierFilter getVerifierFilter()
-	{
-		return new FilterImpl( grammar,
-			new VerificationErrorHandlerImpl() );
+    public VerifierFilter getVerifierFilter() {
+		return new FilterImpl( grammar, new VerificationErrorHandlerImpl() );
     }
 }

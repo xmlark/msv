76d3cc22039069459f54ff423a67587f2345dc46
Bear <Bear@8cecbe3e-ef64-0410-a610-ece5afc49ac1>
2001-10-19 23:52:23 +0000

    implemented the name check.
    git-svn-id: https://svn.dev.java.net/svn/msv/trunk@1240 8cecbe3e-ef64-0410-a610-ece5afc49ac1

---
+++ msv/src/com/sun/msv/reader/trex/ng/AttributeState.java
@@ -0,0 +1,54 @@
+/*
+ * @(#)$Id$
+ *
+ * Copyright 2001 Sun Microsystems, Inc. All Rights Reserved.
+ * 
+ * This software is the proprietary information of Sun Microsystems, Inc.  
+ * Use is subject to license terms.
+ * 
+ */
+package com.sun.msv.reader.trex.ng;
+
+import com.sun.msv.grammar.*;
+
+/**
+ * parses &lt;attribute&gt; pattern.
+ * 
+ * @author <a href="mailto:kohsuke.kawaguchi@sun.com">Kohsuke KAWAGUCHI</a>
+ */
+public class AttributeState extends com.sun.msv.reader.trex.AttributeState
+{
+	private static final String infosetURI = "http://www.w3.org/2000/xmlns";
+	protected void endSelf() {
+		super.endSelf();
+		
+		nameClass.visit( new NameClassVisitor() {
+			public Object onAnyName( AnyNameClass nc ) { return null; }
+			public Object onSimple(SimpleNameClass nc) {
+				if(nc.namespaceURI.equals(infosetURI))
+					reader.reportError( RELAXNGReader.ERR_INFOSET_URI_ATTRIBUTE );
+				
+				if(nc.namespaceURI.length()==0 && nc.localName.equals("xmlns"))
+					reader.reportError( RELAXNGReader.ERR_XMLNS_ATTRIBUTE );
+				return null;
+			}
+			public Object onNsName( NamespaceNameClass nc ) {
+				if(nc.namespaceURI.equals(infosetURI))
+					reader.reportError( RELAXNGReader.ERR_INFOSET_URI_ATTRIBUTE );
+				return null;
+			}
+			public Object onNot( NotNameClass nc ) {
+				nc.child.visit(this);
+				return null;
+			}
+			public Object onDifference( DifferenceNameClass nc ) {
+				nc.nc1.visit(this); nc.nc2.visit(this);
+				return null;
+			}
+			public Object onChoice( ChoiceNameClass nc ) {
+				nc.nc1.visit(this); nc.nc2.visit(this);
+				return null;
+			}
+		});
+	}
+}

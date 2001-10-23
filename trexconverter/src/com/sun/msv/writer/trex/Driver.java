ec7f4c09cc3afdf3daa1804e6c27a29c5b964c11
Bear <Bear@8cecbe3e-ef64-0410-a610-ece5afc49ac1>
2001-10-23 00:54:30 +0000

    removed the dtd option.
    git-svn-id: https://svn.dev.java.net/svn/msv/trunk@1247 8cecbe3e-ef64-0410-a610-ece5afc49ac1

--- trexconverter/src/com/sun/msv/writer/trex/Driver.java
+++ trexconverter/src/com/sun/msv/writer/trex/Driver.java
@@ -10,7 +10,7 @@
 package com.sun.msv.writer.trex;
 
 import com.sun.msv.reader.util.GrammarLoader;
-import com.sun.msv.reader.dtd.DTDReader;
+import com.sun.msv.driver.textui.DebugController;
 import com.sun.msv.grammar.Grammar;
 import java.io.PrintWriter;
 import org.apache.xml.serialize.OutputFormat;
@@ -24,38 +24,23 @@ import org.apache.xerces.jaxp.SAXParserFactoryImpl;
  */
 public class Driver {
 	public static void main( String[] args ) throws Exception {
-		if( args.length<1 ) {
+		
+		if( args.length!=1 ) {
 			System.out.println( localize(MSG_USAGE) );
 			return;
 		}
 		
-		boolean dtd;
-		String schema;
-		
-		if( args[0].equalsIgnoreCase("-dtd") ) {
-			dtd = true;
-			schema = args[1];
-		} else {
-			dtd = false;
-			schema = args[0];
-		}
-		
-		// use Xerces as a parser.
-		SAXParserFactoryImpl pf = new SAXParserFactoryImpl();
-		pf.setNamespaceAware(true);
+		// use Xerces. Since we are using Xerces to serialize XML,
+		// it is a good idea to use Xerces for parsing, too.
+		SAXParserFactoryImpl factory = new SAXParserFactoryImpl();
+		factory.setNamespaceAware(true);
 		
 		// load a grammar.
-		Grammar g;
+		Grammar g = GrammarLoader.loadSchema(
+			args[0],
+			new DebugController( false,false, System.err ),
+			factory );
 		
-		if( dtd ) {
-			g = DTDReader.parse(
-				com.sun.msv.driver.textui.Driver.getInputSource(schema),
-				new com.sun.msv.driver.textui.DebugController( false,false, System.err ) );
-		} else {
-			g = GrammarLoader.loadSchema( schema,
-				new com.sun.msv.driver.textui.DebugController( false,false, System.err ),
-				pf );
-		}
 		if( g==null ) {
 			System.err.println(localize(MSG_GRAMMAR_ERROR));
 			return;

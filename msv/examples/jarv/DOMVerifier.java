4661e698bef667f6b234e124acf8e679c3210074
kk122374 <kk122374@8cecbe3e-ef64-0410-a610-ece5afc49ac1>
2001-10-31 20:33:27 +0000

    moved the DOMVerfier from the test pacakge.
    git-svn-id: https://svn.dev.java.net/svn/msv/trunk@1285 8cecbe3e-ef64-0410-a610-ece5afc49ac1

---
+++ msv/examples/jarv/DOMVerifier.java
@@ -0,0 +1,55 @@
+/*
+ * @(#)$Id$
+ *
+ * Copyright 2001 Sun Microsystems, Inc. All Rights Reserved.
+ * 
+ * This software is the proprietary information of Sun Microsystems, Inc.  
+ * Use is subject to license terms.
+ * 
+ */
+package jarv;
+
+import org.iso_relax.verifier.*;
+import javax.xml.parsers.*;
+import org.w3c.dom.Document;
+import java.io.File;
+
+/**
+ * Uses <a href="http://iso-relax.sourceforge.net/apiDoc/">JARV</a>
+ * to validate DOM documents/subtree.
+ * 
+ * @author <a href="mailto:kohsuke.kawaguchi@sun.com">Kohsuke KAWAGUCHI</a>
+ */
+public class DOMVerifier
+{
+	public static void main( String[] args ) throws Exception {
+		if(args.length<2) {
+			System.out.println("Usage: DOMVerifier <schema> <instance> ...");
+			return;
+		}
+		
+		// setup JARV and compile a schema.
+		VerifierFactory factory = new com.sun.msv.verifier.jarv.TheFactoryImpl();
+		Verifier verifier = factory.compileSchema(args[0]).newVerifier();
+			// instead, you can call factory.newVerifier(args[0])
+			// this will result in the same behavior.
+		
+		// setup JAXP
+		DocumentBuilderFactory domf = DocumentBuilderFactory.newInstance();
+		domf.setNamespaceAware(true);
+		DocumentBuilder builder = domf.newDocumentBuilder();
+		
+		for( int i=1; i<args.length; i++ ) {
+			// parse a document into a DOM.
+			Document dom = builder.parse(new File(args[i]));
+			
+			// performs the validation on the whole tree.
+			// instead, you can pass an Element to the verify method, too.
+			// e.g.,  verifier.verify(dom.getDocumentElement())
+			if(verifier.verify(dom))
+				System.out.println("valid  :"+args[i]);
+			else
+				System.out.println("invalid:"+args[i]);
+		}
+	}
+}

5fdc624805b36e6a3b494cc8c3c862c85656e52e
kk122374 <kk122374@8cecbe3e-ef64-0410-a610-ece5afc49ac1>
2001-11-22 00:59:07 +0000

    implemented a test that tests the uniqueness of built-in type instances.
    git-svn-id: https://svn.dev.java.net/svn/msv/trunk@1322 8cecbe3e-ef64-0410-a610-ece5afc49ac1

---
+++ xsdlib/test/com/sun/msv/datatype/xsd/SerializationTest.java
@@ -0,0 +1,71 @@
+/*
+ * @(#)$Id$
+ *
+ * Copyright 2001 Sun Microsystems, Inc. All Rights Reserved.
+ * 
+ * This software is the proprietary information of Sun Microsystems, Inc.  
+ * Use is subject to license terms.
+ * 
+ */
+package com.sun.msv.datatype.xsd;
+
+import junit.framework.*;
+import java.io.*;
+
+/**
+ * tests serialization of datatypes.
+ * 
+ * @author <a href="mailto:kohsuke.kawaguchi@eng.sun.com">Kohsuke KAWAGUCHI</a>
+ */
+public class SerializationTest extends TestCase
+{
+	public SerializationTest( String name ) { super(name); }
+	
+	public static void main(java.lang.String[] args) {
+		junit.textui.TestRunner.run(suite());
+	}
+	
+	public static Test suite() {
+		return new TestSuite(SerializationTest.class);
+	}
+	
+    // serialize and de-serialize
+    public Object freezeDry( Object dt ) throws Exception {
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        ObjectOutputStream oos = new ObjectOutputStream(bos);
+        
+        // serialize it
+        oos.writeObject(dt);
+        oos.flush();
+        
+        ByteArrayInputStream bis = new ByteArrayInputStream(bos.toByteArray());
+        ObjectInputStream ois = new ObjectInputStream(bis);
+        
+        return ois.readObject();
+    }
+    
+	/** test singleton-ness of built-in datatypes. */
+	public void testSingletonness() throws Exception {
+        for( int i=0; i<Const.builtinTypeNames.length; i++ ) {
+            String name = Const.builtinTypeNames[i];
+            
+            XSDatatype dt = DatatypeFactory.getTypeByName(name);
+            assert(dt!=null);
+            
+            assert("freeze dry test for "+dt.getName(),
+                freezeDry(dt)==dt);  // it must be singleton.
+        }
+    }
+    
+    public void testSingletonness2() throws Exception {
+        // built-in types use their names to keep singleton-ness.
+        // make sure that it won't be deceived by datatypes
+        // whose name is accidentally the same as the built-in type.
+        
+        TypeIncubator ti = new TypeIncubator(StringType.theInstance);
+        ti.addFacet("maxLength","120",false,null);
+        XSDatatype pseudoString = ti.derive("string");
+        
+        assert( freezeDry(pseudoString)!=StringType.theInstance );
+    }
+}

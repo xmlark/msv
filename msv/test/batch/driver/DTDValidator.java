e4426aa5bc586312e58af9df9ac81cc86286d884
kk122374 <kk122374@8cecbe3e-ef64-0410-a610-ece5afc49ac1>
2002-02-13 05:18:25 +0000

    modified not to do a smart system id correction.
    git-svn-id: https://svn.dev.java.net/svn/msv/trunk@1349 8cecbe3e-ef64-0410-a610-ece5afc49ac1

--- msv/test/batch/driver/DTDValidator.java
+++ msv/test/batch/driver/DTDValidator.java
@@ -17,12 +17,11 @@ import java.net.URL;
 public class DTDValidator extends AbstractValidatorExImpl {
 
 	public Grammar parseSchema( InputSource is, GrammarReaderController controller ) throws Exception {
-		is.setSystemId( toURL(is.getSystemId()) );
 		Grammar g = DTDReader.parse(is,controller,new ExpressionPool() );
 		if(g==null)		return null;
 		return g;
 	}
-	
+/*	
 	protected String toURL( String path ) throws Exception {
 		path = new File(path).getAbsolutePath();
 		if (File.separatorChar != '/')
@@ -33,4 +32,5 @@ public class DTDValidator extends AbstractValidatorExImpl {
 //			path = path + "/";
 		return new URL("file", "", path).toExternalForm();
 	}
+*/
 }

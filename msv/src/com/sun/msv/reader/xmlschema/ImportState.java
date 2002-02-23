1377dc9cb11e45e525c151c1e103b415e78674dc
kk122374 <kk122374@8cecbe3e-ef64-0410-a610-ece5afc49ac1>
2002-02-23 22:47:30 +0000

    corrected a wrong handling of the missing namespace attribute.
    git-svn-id: https://svn.dev.java.net/svn/msv/trunk@1356 8cecbe3e-ef64-0410-a610-ece5afc49ac1

--- msv/src/com/sun/msv/reader/xmlschema/ImportState.java
+++ msv/src/com/sun/msv/reader/xmlschema/ImportState.java
@@ -23,12 +23,8 @@ public class ImportState extends ChildlessState {
 		final XMLSchemaReader reader = (XMLSchemaReader)this.reader;
 		
 		// TODO: @schemaLocation is optional.
-		// TODO: @namespace is also optional, but I couldn't get what shall we do if it's absent.
 		String namespace = startTag.getAttribute("namespace");
-		if( namespace==null ) {
-			reader.reportError( reader.ERR_MISSING_ATTRIBUTE, "import", "namespace" );
-			return;	// recover by ignoring this import.
-		}
+		if( namespace==null )   namespace="";
 		
 		if( namespace.equals(reader.currentSchema.targetNamespace) ) {
 			reader.reportError( reader.ERR_IMPORTING_SAME_NAMESPACE );

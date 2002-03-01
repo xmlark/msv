f0efba7c9d5a4e709a27bb72413245abc527ad02
kk122374 <kk122374@8cecbe3e-ef64-0410-a610-ece5afc49ac1>
2002-03-01 00:33:46 +0000

    fixed a bug which causes a strange cyclic reference error.
    git-svn-id: https://svn.dev.java.net/svn/msv/trunk@1366 8cecbe3e-ef64-0410-a610-ece5afc49ac1

--- relames/src/com/sun/msv/schematron/reader/SElementState.java
+++ relames/src/com/sun/msv/schematron/reader/SElementState.java
@@ -2,7 +2,7 @@ package com.sun.msv.schematron.reader;
 
 import com.sun.msv.grammar.Expression;
 import com.sun.msv.reader.GrammarReader;
-import com.sun.msv.reader.trex.ElementState;
+import com.sun.msv.reader.trex.ng.ElementState;
 import com.sun.msv.reader.State;
 import com.sun.msv.util.StartTagInfo;
 import com.sun.msv.schematron.grammar.SElementExp;

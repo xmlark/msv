333e343ca3da1e82d2c718362811d2ea468c50e0
kk122374 <kk122374@8cecbe3e-ef64-0410-a610-ece5afc49ac1>
2002-07-25 00:13:36 +0000

    added more jar files that works with my new environment.
    git-svn-id: https://svn.dev.java.net/svn/msv/trunk@1424 8cecbe3e-ef64-0410-a610-ece5afc49ac1

--- msv/setenv.bat
+++ msv/setenv.bat
@@ -2,3 +2,6 @@
 @set CLASSPATH=%CLASSPATH%;..\..\testharness\bin
 @set CLASSPATH=%CLASSPATH%;..\..\xsdlib\bin
 @set CLASSPATH=%CLASSPATH%;..\..\xsdlib\src
+@set CLASSPATH=%CLASSPATH%;..\..\shared\lib\isorelax.jar
+@set CLASSPATH=%CLASSPATH%;..\..\shared\lib\relaxngDatatype.jar
+@set CLASSPATH=%CLASSPATH%;..\..\shared\lib\xerces.jar

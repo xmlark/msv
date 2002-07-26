6f2f3cc65f1d156d29d44a74c0b2ca96879a4128
kk122374 <kk122374@8cecbe3e-ef64-0410-a610-ece5afc49ac1>
2002-07-26 21:41:05 +0000

    updated to my new environment.
    git-svn-id: https://svn.dev.java.net/svn/msv/trunk@1434 8cecbe3e-ef64-0410-a610-ece5afc49ac1

--- generator/setenv.bat
+++ generator/setenv.bat
@@ -3,3 +3,6 @@ set CLASSPATH=%CLASSPATH%;..\..\msv\bin
 set CLASSPATH=%CLASSPATH%;..\..\msv\src
 set CLASSPATH=%CLASSPATH%;..\..\xsdlib\bin
 set CLASSPATH=%CLASSPATH%;..\..\xsdlib\src
+set CLASSPATH=%CLASSPATH%;c:\kohsuke\sun\msv\shared\lib\relaxngDatatype.jar
+set CLASSPATH=%CLASSPATH%;c:\kohsuke\sun\msv\shared\lib\isorelax.jar
+set CLASSPATH=%CLASSPATH%;c:\kohsuke\sun\msv\shared\lib\xerces.jar

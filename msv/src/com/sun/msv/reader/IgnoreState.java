bce25a7b3e3fffd74e22b17aa983b6cfbbcd71e9
kk122374 <kk122374@8cecbe3e-ef64-0410-a610-ece5afc49ac1>
2003-03-13 20:13:22 +0000

    reformatted the source code
    git-svn-id: https://svn.dev.java.net/svn/msv/trunk@1522 8cecbe3e-ef64-0410-a610-ece5afc49ac1

--- msv/src/com/sun/msv/reader/IgnoreState.java
+++ msv/src/com/sun/msv/reader/IgnoreState.java
@@ -16,30 +16,29 @@ import org.xml.sax.Attributes;
  * 
  * @author <a href="mailto:kohsuke.kawaguchi@eng.sun.com">Kohsuke KAWAGUCHI</a>
  */
-public class IgnoreState extends State
-{
-	// the parent state receives startElement event for this state.
-	// so we have to start from one, rather than zero.
-	private int depth=1;
-	
-	public final void startElement( String namespaceURI, String localName, String qName, Attributes atts )
-	{
-		depth++;
-	}
-	public final void endElement( String namespaceURI, String localName, String qName )
-	{
-		depth--;
-		if(depth==0)	reader.popState();
-	}
-	public final void endDocument()
-	{
-		// assert depth==0
-		reader.popState();
-	}
-	public void characters(char[] buffer, int from, int len )
-	{
-		// ignore literals
-	}
+public class IgnoreState extends State {
+    // the parent state receives startElement event for this state.
+    // so we have to start from one, rather than zero.
+    private int depth = 1;
+
+    public final void startElement(String namespaceURI, String localName, String qName, Attributes atts) {
+        depth++;
+    }
+    
+    public final void endElement(String namespaceURI, String localName, String qName) {
+        depth--;
+        if (depth == 0)
+            reader.popState();
+    }
+    
+    public final void endDocument() {
+        // assert depth==0
+        reader.popState();
+    }
+    
+    public void characters(char[] buffer, int from, int len) {
+        // ignore literals
+    }
 
 }
 	

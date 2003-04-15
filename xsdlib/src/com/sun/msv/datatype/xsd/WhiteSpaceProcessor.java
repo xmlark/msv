fa7e4126d10f9fc03321ac9b6f1511e2784267ca
kk122374 <kk122374@8cecbe3e-ef64-0410-a610-ece5afc49ac1>
2003-04-15 14:49:06 +0000

    modified not to use anonymous classes as a part of serialization.
    git-svn-id: https://svn.dev.java.net/svn/msv/trunk@1537 8cecbe3e-ef64-0410-a610-ece5afc49ac1

--- xsdlib/src/com/sun/msv/datatype/xsd/WhiteSpaceProcessor.java
+++ xsdlib/src/com/sun/msv/datatype/xsd/WhiteSpaceProcessor.java
@@ -20,116 +20,163 @@ import org.relaxng.datatype.DatatypeException;
  * @author <a href="mailto:kohsuke.kawaguchi@eng.sun.com">Kohsuke KAWAGUCHI</a>
  */
 public abstract class WhiteSpaceProcessor implements Serializable {
-	
-	/**
-	 * returns whitespace normalized text.
-	 * behavior varies on what normalization mode is used.
-	 */
-	public abstract String process( String text );
-	
-	/** higher return value indicates tigher constraint */
-	abstract int tightness();
-	
-	/**
-	 * gets the name of the white space processing mode.
-	 * It is one of "preserve","collapse", or "replace".
-	 */
-	public abstract String getName();
-	
-	/**
-	 * returns a WhiteSpaceProcessor object if "whiteSpace" facet is specified.
-	 * Otherwise returns null.
-	 */
-	protected static WhiteSpaceProcessor get( String name ) throws DatatypeException {
-		name = theCollapse.process(name);
-		if( name.equals("preserve") )		return thePreserve;
-		if( name.equals("collapse") )		return theCollapse;
-		if( name.equals("replace") )		return theReplace;
-		
-		throw new DatatypeException( XSDatatypeImpl.localize(
-			XSDatatypeImpl.ERR_INVALID_WHITESPACE_VALUE, name ));
-	}
-	
-	/** returns true if the specified char is a white space character. */
-	protected static final boolean isWhiteSpace( char ch ) {
-		return ch==0x9 || ch==0xA || ch==0xD || ch==0x20;
-	}
-	
-	
-	protected Object readResolve() throws InvalidObjectException {
-		// return the singleton instead of deserialized object.
-		try {
-			return get(getName());
-		} catch( DatatypeException bte ) {
-			throw new InvalidObjectException("Unknown Processing Mode");
-		}
-	}
 
-// short-cut methods
-	public static String replace( String str ) { return theReplace.process(str); }
-	public static String collapse( String str ) { return theCollapse.process(str); }
-	
-/*
-	Actual processor implementation
-*/
-	public final static WhiteSpaceProcessor thePreserve = new WhiteSpaceProcessor() {
-		public String process( String text )	{ return text; }
-		int tightness() { return 0; }
-		public String getName() { return "preserve"; }
-	};
-	
-	public final static WhiteSpaceProcessor theReplace = new WhiteSpaceProcessor() {
-		public String process( String text ) {
-			final int len = text.length();
-			StringBuffer result = new StringBuffer(len);
-			
-			for( int i=0; i<len; i++ ) {
-				char ch = text.charAt(i);
-				if( super.isWhiteSpace(ch) )
-					result.append(' ');
-				else
-					result.append(ch);
-			}
-			
-			return result.toString();		
-		}
-		int tightness() { return 1; }
-		public String getName() { return "replace"; }
-	};
+    /**
+     * returns whitespace normalized text.
+     * behavior varies on what normalization mode is used.
+     */
+    public abstract String process(String text);
 
-	public final static WhiteSpaceProcessor theCollapse= new WhiteSpaceProcessor() {
-		public String process( String text ) {
-			int len = text.length();
-			StringBuffer result = new StringBuffer(len /**enough size*/ );
-			
-			boolean inStripMode = true;
-			
-			for( int i=0; i<len; i++ ) {
-				char ch = text.charAt(i);
-				boolean b = WhiteSpaceProcessor.isWhiteSpace(ch);
-				if( inStripMode && b )
-					continue;	// skip this character
-				
-				inStripMode = b;
-				if( inStripMode )	result.append(' ');
-				else				result.append(ch);
-			}
-			
-			// remove trailing whitespaces
-			len = result.length();
-			if( len>0 && result.charAt(len-1)==' ' )
-				result.setLength(len-1);
-			// whitespaces are already collapsed,
-			// so all we have to do is to remove the last one character
-			// if it's a whitespace.
-			
-			return result.toString();
-		}
-		int tightness() { return 2; }
-		public String getName() { return "collapse"; }
-	};
+    /** higher return value indicates tigher constraint */
+    abstract int tightness();
+
+    /**
+     * gets the name of the white space processing mode.
+     * It is one of "preserve","collapse", or "replace".
+     */
+    public abstract String getName();
+
+    /**
+     * returns a WhiteSpaceProcessor object if "whiteSpace" facet is specified.
+     * Otherwise returns null.
+     */
+    protected static WhiteSpaceProcessor get(String name) throws DatatypeException {
+        name = theCollapse.process(name);
+        if (name.equals("preserve"))    return thePreserve;
+        if (name.equals("collapse"))    return theCollapse;
+        if (name.equals("replace"))     return theReplace;
+
+        throw new DatatypeException(XSDatatypeImpl.localize(XSDatatypeImpl.ERR_INVALID_WHITESPACE_VALUE, name));
+    }
+
+    /** returns true if the specified char is a white space character. */
+    protected static final boolean isWhiteSpace(char ch) {
+        return ch == 0x9 || ch == 0xA || ch == 0xD || ch == 0x20;
+    }
+
+    protected Object readResolve() throws InvalidObjectException {
+        // return the singleton instead of deserialized object.
+        try {
+            return get(getName());
+        } catch (DatatypeException bte) {
+            throw new InvalidObjectException("Unknown Processing Mode");
+        }
+    }
+
+    // short-cut methods
+    public static String replace(String str) {
+        return theReplace.process(str);
+    }
+    public static String collapse(String str) {
+        return theCollapse.process(str);
+    }
+
+    public final static WhiteSpaceProcessor thePreserve = new Preserve();
+    public final static WhiteSpaceProcessor theReplace = new Replace();
+    public final static WhiteSpaceProcessor theCollapse = new Collapse();
+
+    /*
+    	Actual processor implementation
+    */
+    private static class Preserve extends WhiteSpaceProcessor
+    {
+        public String process(String text) {
+            return text;
+        }
+        int tightness() {
+            return 0;
+        }
+        public String getName() {
+            return "preserve";
+        }
+        
+        // serialization support
+        private static final long serialVersionUID = 1;
+    };
+    
+
+    private static class Replace extends WhiteSpaceProcessor
+    {
+        public String process(String text) {
+            final int len = text.length();
+            StringBuffer result = new StringBuffer(len);
+
+            for (int i = 0; i < len; i++) {
+                char ch = text.charAt(i);
+                if (super.isWhiteSpace(ch))
+                    result.append(' ');
+                else
+                    result.append(ch);
+            }
+
+            return result.toString();
+        }
+        int tightness() {
+            return 1;
+        }
+        public String getName() {
+            return "replace";
+        }
+        
+        // serialization support
+        private static final long serialVersionUID = 1;
+    };
+
+    private static class Collapse extends WhiteSpaceProcessor
+    {
+        public String process(String text) {
+            int len = text.length();
+            StringBuffer result = new StringBuffer(len /**enough size*/
+            );
+
+            boolean inStripMode = true;
+
+            for (int i = 0; i < len; i++) {
+                char ch = text.charAt(i);
+                boolean b = WhiteSpaceProcessor.isWhiteSpace(ch);
+                if (inStripMode && b)
+                    continue; // skip this character
+
+                inStripMode = b;
+                if (inStripMode)
+                    result.append(' ');
+                else
+                    result.append(ch);
+            }
+
+            // remove trailing whitespaces
+            len = result.length();
+            if (len > 0 && result.charAt(len - 1) == ' ')
+                result.setLength(len - 1);
+            // whitespaces are already collapsed,
+            // so all we have to do is to remove the last one character
+            // if it's a whitespace.
+
+            return result.toString();
+        }
+        int tightness() {
+            return 2;
+        }
+        public String getName() {
+            return "collapse";
+        }
+
+        // serialization support
+        private static final long serialVersionUID = 1;
+    };
+    
+    
+    /**
+     * @deprecated
+     */
+    private void backwardCompatibiliyHook() {
+        // for backward compatibility: older version of XSDLib
+        // were using inner classes, and those need to resolve correctly
+        new Preserve() {};
+        new Collapse() {};
+        new Replace() {};
+    }
 
     // serialization support
-    private static final long serialVersionUID = 1;    
+    private static final long serialVersionUID = 1;
 }
-

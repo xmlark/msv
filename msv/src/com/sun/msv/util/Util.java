6fd43ef00369d93fe3bdf8ede542f9430c350873
kohsuke <kohsuke@8cecbe3e-ef64-0410-a610-ece5afc49ac1>
2003-10-24 18:54:03 +0000

    added "which" function.
    git-svn-id: https://svn.dev.java.net/svn/msv/trunk@1606 8cecbe3e-ef64-0410-a610-ece5afc49ac1

--- msv/src/com/sun/msv/util/Util.java
+++ msv/src/com/sun/msv/util/Util.java
@@ -77,4 +77,30 @@ public class Util
         
         return false;
     }
+
+
+    public static String which( Class clazz ) {
+        return which( clazz.getName(), clazz.getClassLoader() );
+    }
+
+    /**
+     * Search the specified classloader for the given classname.
+     *
+     * @param classname the fully qualified name of the class to search for
+     * @param loader the classloader to search
+     * @return the source location of the resource, or null if it wasn't found
+     */
+    public static String which(String classname, ClassLoader loader) {
+
+        String classnameAsResource = classname.replace('.', '/') + ".class";
+        
+        if( loader==null )  loader = ClassLoader.getSystemClassLoader();
+        
+        URL it = loader.getResource(classnameAsResource);
+        if (it != null) {
+            return it.toString();
+        } else {
+            return null;
+        }
+    }
 }

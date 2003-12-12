c81c5fdb4638f71b15525759199330ed98113939
kohsuke <kohsuke@8cecbe3e-ef64-0410-a610-ece5afc49ac1>
2003-12-12 19:46:03 +0000

    added a small test program.
    git-svn-id: https://svn.dev.java.net/svn/msv/trunk@1618 8cecbe3e-ef64-0410-a610-ece5afc49ac1

---
+++ msv/test/SerialTest.java
@@ -0,0 +1,39 @@
+/*
+ * @(#)$Id$
+ *
+ * Copyright 2001 Sun Microsystems, Inc. All Rights Reserved.
+ * 
+ * This software is the proprietary information of Sun Microsystems, Inc.  
+ * Use is subject to license terms.
+ * 
+ */
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+import com.sun.msv.datatype.xsd.StringType;
+import com.sun.msv.grammar.ExpressionPool;
+
+
+/**
+ * Test program for serialization compatibility of {@link ExpressionPool}. 
+ * 
+ * @author
+ *     Kohsuke Kawaguchi (kohsuke.kawaguchi@sun.com)
+ */
+public class SerialTest {
+    public static void main(String[] args) throws Exception {
+        if( args[0].equals("write") ) {
+        	ExpressionPool pool = new ExpressionPool();
+            pool.createAnyString();
+            pool.createChoice( pool.createData(StringType.theInstance), pool.createEpsilon() );
+        
+            ObjectOutputStream oos = new ObjectOutputStream(System.out);
+            oos.writeObject(pool);
+            oos.close();
+        } else {
+            ObjectInputStream ois = new ObjectInputStream(System.in);
+            ExpressionPool pool = (ExpressionPool)ois.readObject();
+            System.out.println(pool);
+        }
+    }
+}

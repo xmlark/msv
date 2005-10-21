e583a6d375cd5320515d596e85d30d36da12b93e
kohsuke <kohsuke@8cecbe3e-ef64-0410-a610-ece5afc49ac1>
2005-10-21 23:01:20 +0000

    fixed a bug where an inline element couldn't have @occurs
    git-svn-id: https://svn.dev.java.net/svn/msv/trunk@1698 8cecbe3e-ef64-0410-a610-ece5afc49ac1

--- msv/src/com/sun/msv/reader/relax/RELAXReader.java
+++ msv/src/com/sun/msv/reader/relax/RELAXReader.java
@@ -22,6 +22,7 @@ import com.sun.msv.reader.GrammarReaderController;
 import com.sun.msv.reader.SequenceState;
 import com.sun.msv.reader.State;
 import com.sun.msv.reader.TerminalState;
+import com.sun.msv.reader.relax.core.InlineElementState;
 import com.sun.msv.reader.datatype.xsd.FacetState;
 import com.sun.msv.util.StartTagInfo;
 
@@ -89,7 +90,8 @@ public abstract class RELAXReader extends GrammarReader
             state instanceof SequenceState
         ||    state instanceof ElementRefState
         ||    state instanceof HedgeRefState
-        ||    state instanceof ChoiceState;
+        ||    state instanceof ChoiceState
+        ||      state instanceof InlineElementState;
     }
 
     protected Expression interceptExpression( State state, Expression exp )

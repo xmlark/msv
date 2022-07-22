65a2dc1a71b10ebdda04d2be089c970b8668a9af
Svante Schubert <svante.schubert@gmail.com>
2022-07-22 17:21:46 +0200

    Fixing command-line for trexconverter by fixing localized messages

--- trexconverter/src/main/java/com/sun/msv/writer/trex/Driver.java
+++ trexconverter/src/main/java/com/sun/msv/writer/trex/Driver.java
@@ -2,10 +2,10 @@
  * @(#)$Id$
  *
  * Copyright 2001 Sun Microsystems, Inc. All Rights Reserved.
- * 
- * This software is the proprietary information of Sun Microsystems, Inc.  
+ *
+ * This software is the proprietary information of Sun Microsystems, Inc.
  * Use is subject to license terms.
- * 
+ *
  */
 package com.sun.msv.writer.trex;
 
@@ -19,33 +19,33 @@ import org.apache.xerces.jaxp.SAXParserFactoryImpl;
 
 /**
  * converts any supported languages into the equivalent TREX grammar.
- * 
+ *
  * @author <a href="mailto:kohsuke.kawaguchi@eng.sun.com">Kohsuke KAWAGUCHI</a>
  */
 public class Driver {
 	public static void main( String[] args ) throws Exception {
-		
+
 		if( args.length!=1 ) {
 			System.out.println( localize(MSG_USAGE) );
 			return;
 		}
-		
+
 		// use Xerces. Since we are using Xerces to serialize XML,
 		// it is a good idea to use Xerces for parsing, too.
 		SAXParserFactoryImpl factory = new SAXParserFactoryImpl();
 		factory.setNamespaceAware(true);
-		
+
 		// load a grammar.
 		Grammar g = GrammarLoader.loadSchema(
 			args[0],
 			new DebugController( false,false, System.err ),
 			factory );
-		
+
 		if( g==null ) {
 			System.err.println(localize(MSG_GRAMMAR_ERROR));
 			return;
 		}
-		
+
 		TREXWriter writer = new TREXWriter();
 		// use XMLSerializer of Apache to serialize SAX event into plain text.
 		// OutputFormat specifies "pretty printing".
@@ -55,11 +55,11 @@ public class Driver {
 		// visit TREXGrammar and generate its XML representation.
 		writer.write( g );
 	}
-	
-	
+
+
 	public static String localize( String propertyName, Object[] args ) {
 		String format = java.util.ResourceBundle.getBundle(
-			"com.sun.msv.writer.relaxng.Messages").getString(propertyName);
+			"com.sun.msv.writer.trex.Messages").getString(propertyName);
 	    return java.text.MessageFormat.format(format, args );
 	}
 	public static String localize( String prop ) {
@@ -71,7 +71,7 @@ public class Driver {
 	public static String localize( String prop, Object arg1, Object arg2 ) {
 		return localize(prop,new Object[]{arg1,arg2});
 	}
-	
+
 	private static final String MSG_USAGE = // arg:0
 		"Driver.Usage";
 	private static final String MSG_GRAMMAR_ERROR = // arg:0

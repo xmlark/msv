7f6059f1186c9db023b9cb3724055712d173d258
Svante Schubert <svante.schubert@gmail.com>
2023-02-14 10:06:12 +0100

    Linux requires adjustment to test references and tests..

--- msv/src/test/java/com/sun/msv/integrationtest/JarUbl12CreditNoteExample1_IT.java
+++ msv/src/test/java/com/sun/msv/integrationtest/JarUbl12CreditNoteExample1_IT.java
@@ -29,23 +29,23 @@
  */
 package com.sun.msv.integrationtest;
 
+import static com.sun.msv.integrationtest.JarRunner.getUserDirectoryUrlPath;
 import java.io.File;
 import java.util.logging.Logger;
 import org.junit.Test;
 
 public class JarUbl12CreditNoteExample1_IT {
 
-    private static final Logger LOG = Logger.getLogger(JarUbl12CreditNoteExample1_IT.class.getName());
-    private static final String TEST_DIR = System.getProperty("user.dir").replace('\\', '/');
+    private static final Logger LOG = Logger.getLogger(JarUbl12CreditNoteExample1_IT.class.getName());    
     private static final String[] TEST_PARAMETERS = {"-warning",
         "src" + File.separator + "test" + File.separator + "resources" + File.separator + "grammar" + File.separator + "ubl1.2" + File.separator + "xsd" + File.separator + "maindoc" + File.separator + "UBL-CreditNote-2.1.xsd",
         "src" + File.separator + "test" + File.separator + "resources" + File.separator + "files" + File.separator + "ubl1.2" + File.separator + "ubl-tc434-creditnote1.xml"};
     private static final String[] EXCPECTED_OUTPUT_LINES = {
         "start parsing a grammar.",
         "\"anyType\" is implicitly used as the content model of this element. Is this your intention? If so, please consider to write it explicitly as type=\"anyType\".",
-        "  258:48@file:/" + TEST_DIR + "/src/test/resources/grammar/ubl1.2/xsd/common/UBL-XAdESv132-2.1.xsd",
+        "  258:48@file:" + getUserDirectoryUrlPath() + "/src/test/resources/grammar/ubl1.2/xsd/common/UBL-XAdESv132-2.1.xsd",
         "\"anyType\" is implicitly used as the content model of this element. Is this your intention? If so, please consider to write it explicitly as type=\"anyType\".",
-        "  321:47@file:/" +  TEST_DIR + "/src/test/resources/grammar/ubl1.2/xsd/common/UBL-XAdESv132-2.1.xsd",
+        "  321:47@file:" + getUserDirectoryUrlPath() + "/src/test/resources/grammar/ubl1.2/xsd/common/UBL-XAdESv132-2.1.xsd",
         "validating src" + File.separator + "test" + File.separator + "resources" + File.separator + "files" + File.separator + "ubl1.2" + File.separator + "ubl-tc434-creditnote1.xml",
         "the document is valid."
     };
